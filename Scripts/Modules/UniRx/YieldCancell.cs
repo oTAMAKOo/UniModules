36630fb4ad5267ffc0f08acd3f6b01564155b72d
oTAMAKOo <oTAMAKOo@hotmail.co.jp>
2018-09-08 06:02:25 +0900

    .Net4.6だとUnityのバグでgoogle-gdataでエラーが発生する為.Net3.5に戻す

--- Scripts/Modules/UniRx/YieldCancell.cs
+++ Scripts/Modules/UniRx/YieldCancell.cs
@@ -5,19 +5,62 @@ using UniRx;
 
 namespace Modules.UniRxExtension
 {
+    #if !(NETFX_CORE || NET_4_6 || NET_STANDARD_2_0 || UNITY_WSA_10_0)
+
     public class YieldCancell : IDisposable
     {
         //----- params -----
 
         //----- field -----
-        
+
+        private BooleanDisposable cancelDisposable = null;
+        private CancellationToken cancellationToken;
+
+        //----- property -----
+
+        public bool IsCancelled { get { return cancellationToken.IsCancellationRequested; } }
+
+        public CancellationToken Token { get { return cancellationToken; } }
+
+        //----- method -----
+
+        public YieldCancell()
+        {
+            cancelDisposable = new BooleanDisposable();
+            cancellationToken = new CancellationToken(cancelDisposable);
+        }
+
+        ~YieldCancell()
+        {
+            Dispose();
+        }
+
+        public void Dispose()
+        {
+            if (cancelDisposable != null)
+            {
+                cancelDisposable.Dispose();
+            }
+
+            GC.SuppressFinalize(this);
+        }
+    }
+
+    #else
+
+    public class YieldCancell : IDisposable
+    {
+        //----- params -----
+
+        //----- field -----
+
         private CancellationTokenSource cancellationToken = null;
 
         //----- property -----
 
-        public CancellationToken Token { get { return cancellationToken.Token; } }
-
         public bool IsCancelled { get { return cancellationToken.IsCancellationRequested; } }
+
+        public CancellationToken Token { get { return cancellationToken.Token; } }
 
         //----- method -----
 
@@ -42,4 +85,7 @@ namespace Modules.UniRxExtension
             GC.SuppressFinalize(this);
         }
     }
+
+    #endif
+
 }
