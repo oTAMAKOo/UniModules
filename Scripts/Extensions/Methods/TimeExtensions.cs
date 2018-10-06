8cd2a06d15ae892276543d8b4f9367eaba781b56
oTAMAKOo <oTAMAKOo@hotmail.co.jp>
2018-10-07 06:04:04 +0900

    時間周りの拡張コード微修正

--- Scripts/Extensions/Methods/TimeExtensions.cs
+++ Scripts/Extensions/Methods/TimeExtensions.cs
@@ -12,31 +12,25 @@ namespace Extensions
             return to - from;
         }
 
+        /// <summary> 指定されたTimeSpanより短いか. </summary>
         public static bool IsShorterThan(this TimeSpan timeSpan, TimeSpan amount)
         {
             return timeSpan > amount;
         }
 
+        /// <summary> 指定されたTimeSpanより長いか. </summary>
         public static bool IsLongerThan(this TimeSpan timeSpan, TimeSpan amount)
         {
             return timeSpan < amount;
         }
 
-        /// <summary>
-        /// 現在時刻からUnixTime (Milliseconds)を計算.
-        /// </summary>
-        /// <param name="dateTime"></param>
-        /// <returns></returns>
+        /// <summary> 現在時刻からUnixTime (Milliseconds)を計算. </summary>
         public static long ToUnixTime(this DateTime dateTime)
         {
             return (long)dateTime.ToUniversalTime().Subtract(UNIX_EPOCH).TotalMilliseconds;
         }
 
-        /// <summary>
-        /// UNIX時間からDateTimeに変換.
-        /// </summary>
-        /// <param name="unixTime"></param>
-        /// <returns></returns>
+        /// <summary> UNIX時間からDateTimeに変換. </summary>
         public static DateTime UnixTimeToDateTime(this long unixTime)
         {
             return UNIX_EPOCH.AddMilliseconds(unixTime);
