1210ad109f8a9b9d62995d83b57e075b5540904e
oTAMAKOo <oTAMAKOo@hotmail.co.jp>
2018-12-14 03:42:13 +0900

    Prefabインスタンス更新時にSpriteが破棄されるので再適用

---
+++ Scripts/Modules/AtlasTexture/AtlasTexture/Editor/AtlasTextureUpdateHook.cs
@@ -0,0 +1,43 @@
+﻿
+using UnityEngine;
+using UnityEditor;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using UniRx;
+using Extensions;
+using Extensions.Devkit;
+using Unity.Linq;
+
+namespace Modules.Atlas
+{
+    public static class AtlasTextureUpdateHook
+    {
+        //----- params -----
+
+        //----- field -----
+
+        //----- property -----
+
+        //----- method -----
+
+        [InitializeOnLoadMethod]
+        public static void InitializeOnLoadMethod()
+        {
+            PrefabUtility.prefabInstanceUpdated += ApplyAtlasTextureImage;
+        }
+
+        private static void ApplyAtlasTextureImage(GameObject instance)
+        {
+            var atlasTextureImages = instance.DescendantsAndSelf().OfComponent<AtlasTextureImage>();
+
+            foreach (var atlasTextureImage in atlasTextureImages)
+            {
+                if (atlasTextureImage.Atlas != null)
+                {
+                    atlasTextureImage.Apply();
+                }
+            }
+        }
+    }
+}
