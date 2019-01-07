a27511df1eb4004d228b464425c5ca15394efa81
oTAMAKOo <oTAMAKOo@hotmail.co.jp>
2019-01-08 00:00:48 +0900

    MessagePackのログを出力するように修正

--- Scripts/Modules/MessagePack/Editor/MessagePackCodeGenerator.cs
+++ Scripts/Modules/MessagePack/Editor/MessagePackCodeGenerator.cs
@@ -1,7 +1,11 @@
-﻿﻿﻿﻿﻿
-using UnityEngine;
+﻿
 using UnityEditor;
+using System;
+using System.Diagnostics;
+using System.Text;
 using Extensions;
+
+using Debug = UnityEngine.Debug;
 
 namespace Modules.MessagePack
 {
@@ -22,25 +26,79 @@ namespace Modules.MessagePack
         {
             var messagePackConfig = MessagePackConfig.Instance;
 
+            var csprojName = string.Empty;
+
             var projectFolder = UnityPathUtility.GetProjectFolderPath();
-            var projectName = UnityPathUtility.GetProjectName();
-                
-            var csproj = PathUtility.Combine(projectFolder, string.Format("{0}{1}", projectName, CSProjExtension));
+
+            #if UNITY_2018_3_OR_NEWER
+
+            csprojName = "Assembly-CSharp";
+
+            #else
+
+            csprojName = UnityPathUtility.GetProjectName();
+
+            #endif
+
+            var csproj = PathUtility.Combine(projectFolder, string.Format("{0}{1}", csprojName, CSProjExtension));
             var generatePath = PathUtility.Combine(messagePackConfig.ScriptExportDir, messagePackConfig.ExportScriptName);
 
-            var option = string.Format("--input {0} --output {1}", csproj, generatePath);
+            var arguments = string.Format("--input {0} --output {1}", csproj, generatePath);
 
             if (EditorUserBuildSettings.development)
             {
-                option += " --usemapmode";
+                arguments += " --usemapmode";
             }
 
-            // 実行ファイルを指定して実行
-            var compileProcess = System.Diagnostics.Process.Start(messagePackConfig.CompilerPath, option);
+            try
+            {
+                // 実行ファイルを指定して実行.
+                using (var compileProcess = new Process())
+                {
+                    compileProcess.StartInfo.FileName = messagePackConfig.CompilerPath;
 
-            compileProcess.WaitForExit();
+                    compileProcess.StartInfo.Arguments = arguments;
 
-            Debug.LogFormat("Generate: {0}", generatePath);
+                    var compileLog = new StringBuilder();
+
+                    // 標準出力をリダイレクト.
+                    compileProcess.StartInfo.RedirectStandardOutput = true;
+
+                    // シェル実行しない.
+                    compileProcess.StartInfo.UseShellExecute = false;
+
+                    // ウィンドウ非表示.
+                    compileProcess.StartInfo.CreateNoWindow = false;
+
+                    DataReceivedEventHandler processOutputDataReceived = (sender, e) =>
+                    {
+                        compileLog.AppendLine(e.Data).AppendLine();
+                    };
+
+                    compileProcess.OutputDataReceived += processOutputDataReceived;
+
+                    //起動.
+                    compileProcess.Start();
+
+                    compileProcess.BeginOutputReadLine();
+
+                    // 結果待ち.
+                    compileProcess.WaitForExit();
+
+                    if (compileProcess.ExitCode == 1)
+                    {
+                        Debug.LogErrorFormat("MessagePack code geneation failed.\n\n{0}", compileLog.ToString());
+                    }
+                    else
+                    {
+                        Debug.LogFormat("Generate: {0}", generatePath);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Debug.LogError(e.Message + "\n" + e.StackTrace);
+            }
         }
     }
 }
