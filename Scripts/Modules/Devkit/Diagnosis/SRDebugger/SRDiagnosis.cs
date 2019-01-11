3269dc80e6cffaeae1c1d324b62c53d7fec743d0
oTAMAKOo <oTAMAKOo@hotmail.co.jp>
2019-01-12 05:19:25 +0900

    無駄な警告で色が変わってしまうの抑制

--- Scripts/Modules/Devkit/Diagnosis/SRDebugger/SRDiagnosis.cs
+++ Scripts/Modules/Devkit/Diagnosis/SRDebugger/SRDiagnosis.cs
@@ -96,6 +96,21 @@ namespace Modules.Devkit.Diagnosis.SRDebugger
         {
             var changeColor = true;
 
+            //----- TODO: Unity2018.2以降で無駄な警告が出るので、修正されるまで除外. -----
+
+            var ignoreUnity2018_2Warnings = new string[]
+            {
+                "Your multi-scene setup may be improved by tending to the following issues",
+                "Your current multi-scene setup has inconsistent Lighting settings",
+            };
+
+            foreach (var ignore in ignoreUnity2018_2Warnings)
+            {
+                if (logInfo.Condition.StartsWith(ignore)) { return; }
+            }
+            
+            //-----------------------------------------------------------------------------------
+
             if(currentLogType.HasValue)
             {
                 var priority = LogPriority.IndexOf(x => x == logInfo.Type);
