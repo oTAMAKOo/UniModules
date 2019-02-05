fc1b6f3855c7eae345f6519ae2d90c5f20d8650f
竹内誠 <makoto.takeuchi@drecom.co.jp>
2019-02-05 20:48:03 +0900

    SRDebugger系修正

--- Scripts/Modules/Devkit/Diagnosis/SRDebugger/SRDiagnosis.cs
+++ Scripts/Modules/Devkit/Diagnosis/SRDebugger/SRDiagnosis.cs
@@ -57,9 +57,11 @@ namespace Modules.Devkit.Diagnosis.SRDebugger
             {
                 // 一旦アクティブにならないとログ収集しないので一瞬アクティブ化.
                 SRDebug.Instance.ShowDebugPanel();
-                SRDebug.Instance.HideDebugPanel();
 
-                UnityUtility.SetActive(blockCollider, SRDebug.Instance.IsDebugPanelVisible);
+                // 1フレーム待たないとSRDebuggerの当たり判定が残る為次のフレームで非表示化.
+                Observable.NextFrame().Subscribe(_ => SRDebug.Instance.HideDebugPanel()).AddTo(this);
+
+                UnityUtility.SetActive(blockCollider, false);
 
                 SRDebug.Instance.PanelVisibilityChanged += x => { UnityUtility.SetActive(blockCollider, x); };
 
