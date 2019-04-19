01b7633b753e61b314ecda13a90ccc8b934d5e43
竹内誠 <makoto.takeuchi@drecom.co.jp>
2019-04-19 10:07:51 +0900

    Revert "MessagePackの処理修正"
    This reverts commit dd2dd58a99a7b0977cf003f44ac7c5e47cc4b3e5.

--- Scripts/Modules/MessagePack/Editor/MessagePackCodeGenerator.cs
+++ Scripts/Modules/MessagePack/Editor/MessagePackCodeGenerator.cs
@@ -45,13 +45,7 @@ namespace Modules.MessagePack
             var csproj = PathUtility.Combine(projectFolder, string.Format("{0}{1}", csprojName, CSProjExtension));
             var generatePath = PathUtility.Combine(messagePackConfig.ScriptExportDir, messagePackConfig.ExportScriptName);
 
-            var arguments = string.Format(" --input {0} --output {1}", csproj, generatePath);
-
-            // ※ Releaseビルド時はコードジェネレータで生成した物を使用する.
-            if (EditorUserBuildSettings.development)
-            {
-                arguments += " --usemapmode";
-            }
+            var arguments = string.Format(" --input {0} --output {1} --usemapmode", csproj, generatePath);
 
             try
             {
