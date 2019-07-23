5c54e985f94bdc722e3b5914cda2c35a43e312b9
竹内誠 <makoto.takeuchi@drecom.co.jp>
2019-07-23 22:41:13 +0900

    WebView周りの処理統合

--- Scripts/Modules/Devkit/WebView/Editor/EditorWebViewWindow.cs
+++ Scripts/Modules/Devkit/WebView/Editor/EditorWebViewWindow.cs
@@ -3,9 +3,13 @@ using UnityEngine;
 using UnityEditor;
 using UnityEditorInternal;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 using UniRx;
 using Extensions;
+
+using Object = UnityEngine.Object;
 
 namespace Modules.Devkit.WebView
 {
@@ -13,19 +17,43 @@ namespace Modules.Devkit.WebView
     {
         //----- params -----
 
-        private readonly BindingFlags BindingFlags = BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance | BindingFlags.FlattenHierarchy;
+        private enum WebViewMethodType
+        {
+            Show,
+            Hide,
+            Back,
+            Reload,
+            Forward,
+            SetFocus,
+            InitWebView,
+            SetSizeAndPosition,
+            SetHostView,
+            AllowRightClickMenu,
+            SetDelegateObject,
+            ExecuteJavascript,
+            LoadURL,
+            HasApplicationFocus,
+            SetApplicationFocus,
+        }
+
+        private static readonly BindingFlags BindingFlags = BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance | BindingFlags.FlattenHierarchy;
 
         //----- field -----
-
-        [SerializeField]
+        
         private string url = null;
 
         private LifetimeDisposable lifetimeDisposable = new LifetimeDisposable();
 
-        private WebViewHook webView = null;
-        private FieldInfo parentField = null;
-        private MethodInfo parentRepaintMethod = null;
-        private bool gotFocus = false;
+        private Object webView = null;
+
+        private Subject<string> onLocationChanged = null;
+        private Subject<string> onLoadError = null;
+        private Subject<Unit> onInitScripting = null;
+
+        private static Type webViewType = null;
+        private static Dictionary<WebViewMethodType, MethodInfo> webViewMethods = null;
+        private static FieldInfo parentField = null;
+        private static Func<Rect, Rect> unclipMethod = null;
 
         [NonSerialized]
         private bool initialized = false;
@@ -35,6 +63,39 @@ namespace Modules.Devkit.WebView
         public string Url { get { return url; } }
 
         //----- method -----
+
+        static EditorWebViewWindow()
+        {
+            var unityEditorAssembly = Assembly.Load("UnityEditor.dll");
+
+            webViewType = unityEditorAssembly.GetType("UnityEditor.WebView");
+
+            Action<WebViewMethodType> SetWebViewMethod = (methodType) =>
+            {
+                var methodName = Enum.GetName(typeof(WebViewMethodType), methodType);
+                var method = webViewType.GetMethod(methodName, BindingFlags);
+
+                webViewMethods.Add(methodType, method);
+            };
+
+            webViewMethods = new Dictionary<WebViewMethodType, MethodInfo>();
+
+            var webViewMethodTypes = Enum.GetValues(typeof(WebViewMethodType)).Cast<WebViewMethodType>();
+
+            foreach (var webViewMethodType in webViewMethodTypes)
+            {
+                SetWebViewMethod(webViewMethodType);
+            }
+
+            var hostViewType = unityEditorAssembly.GetType("UnityEditor.HostView");
+
+            parentField = typeof(EditorWindow).GetField("m_Parent", BindingFlags);
+
+            unclipMethod = (Func<Rect, Rect>)Delegate.CreateDelegate(
+                 typeof(Func<Rect, Rect>), typeof(GUI).Assembly.GetTypes()
+                 .First(x => x.Name == "GUIClip")
+                 .GetMethod("Unclip", BindingFlags.Static | BindingFlags.Public, null, new Type[] { typeof(Rect) }, null));
+        }
 
         public static T Open<T>(string title, string url) where T : EditorWebViewWindow
         {
@@ -57,59 +118,38 @@ namespace Modules.Devkit.WebView
 
             InitWebView();
 
-            webView.LoadURL(url);
-
-            var hostViewType = Assembly.Load("UnityEditor.dll").GetType("UnityEditor.HostView");
-
-            parentField = typeof(EditorWindow).GetField("m_Parent", BindingFlags);
-            parentRepaintMethod = hostViewType.GetMethod("Repaint", BindingFlags);
+            LoadURL(url);
 
             initialized = true;
-        }
-
-        private void InitWebView()
-        {
-            if (webView != null) { return; }
-
-            webView = new WebViewHook();
-
-            webView.Hook(this);
-
-            webView.OnLocationChangedAsObservable()
-                .Subscribe(x =>
-                           {
-                               url = x;
-                               Repaint();
-                           })
-                .AddTo(lifetimeDisposable.Disposable);
         }
 
         void OnBecameInvisible()
         {
             if (webView != null)
             {
-                webView.Detach();
+                DetachHostView();
+            }
+        }
+        
+        void OnDisable()
+        {
+            if (webView != null)
+            {
+                DetachHostView();
             }
         }
 
-        void OnEnable()
+        void OnDestroy()
         {
-            EditorApplication.update += TrackFocusState;
-        }
-
-        void OnDisable()
-        {
-            EditorApplication.update -= TrackFocusState;
+            UnityEngine.Object.DestroyImmediate(webView);
         }
 
         void OnFocus()
         {
             if (webView != null)
             {
-                webView.SetFocus(true);
+                SetFocus(true);
             }
-
-            OnGotFocus();
 
             Repaint();
         }
@@ -118,7 +158,7 @@ namespace Modules.Devkit.WebView
         {
             if (webView != null)
             {
-                webView.SetFocus(false);
+                SetFocus(false);
             }
 
             InternalEditorUtility.RepaintAllViews();
@@ -137,21 +177,21 @@ namespace Modules.Devkit.WebView
 
                 if (GUILayout.Button(backIcon, EditorStyles.toolbarButton, GUILayout.Width(24f)))
                 {
-                    webView.Back();
+                    Back();
                 }
 
                 var forwardIcon = EditorGUIUtility.IconContent("Profiler.NextFrame");
 
                 if (GUILayout.Button(forwardIcon, EditorStyles.toolbarButton, GUILayout.Width(24f)))
                 {
-                    webView.Forward();
+                    Forward();
                 }
 
                 var reloadIcon = EditorGUIUtility.IconContent("LookDevResetEnv");
 
                 if (GUILayout.Button(reloadIcon, EditorStyles.toolbarButton, GUILayout.Width(24f)))
                 {
-                    webView.Reload();
+                    Reload();
                 }
 
                 EditorGUI.BeginChangeCheck();
@@ -160,8 +200,8 @@ namespace Modules.Devkit.WebView
 
                 if (EditorGUI.EndChangeCheck())
                 {
-                    webView.LoadURL(newUrl);
-                    webView.SetApplicationFocus(true);
+                    LoadURL(newUrl);
+                    SetApplicationFocus(true);
 
                     GUI.FocusControl(string.Empty);
                 }
@@ -169,7 +209,13 @@ namespace Modules.Devkit.WebView
 
             if (Event.current.type == EventType.Repaint)
             {
-                webView.OnGUI(new Rect(0, 20, position.width, position.height - 20));
+                if (webView == null) { return; }
+
+                SetHostView();
+
+                var rect = new Rect(0, 20, position.width, position.height - 20);
+
+                SetSizeAndPosition(unclipMethod(rect));
             }
         }
 
@@ -177,61 +223,235 @@ namespace Modules.Devkit.WebView
         {
             if (webView == null) { return; }
 
-            webView.LoadURL(url);
-        }
-
-        public void Reload()
-        {
-            if (webView == null) { return; }
-
-            webView.Reload();
+            LoadURL(url);
         }
         
         public void Refresh()
         {
-            webView.Hide();
-            webView.Show();
+            HideWebView();
+            ShowWebView();
         }
 
-        private void OnGotFocus()
+        #region WebView
+
+        private void InitWebView()
         {
-            gotFocus = true;
+            if (webView != null) { return; }
 
-            Focus();
+            webView = ScriptableObject.CreateInstance(webViewType);
+            webView.hideFlags = HideFlags.HideAndDontSave;
 
-            if (parentField != null && parentRepaintMethod != null)
-            {
-                var parent = parentField.GetValue(this);
+            Invoke(WebViewMethodType.InitWebView, parentField.GetValue(this), 0, 0, 1, 1, false);
+            Invoke(WebViewMethodType.SetDelegateObject, this);
+            Invoke(WebViewMethodType.AllowRightClickMenu, true);
 
-                parentRepaintMethod.Invoke(parent, null);
-            }
+            OnLocationChangedAsObservable()
+                .Subscribe(x =>
+                    {
+                        url = x;
+                        Repaint();
+                    })
+                .AddTo(lifetimeDisposable.Disposable);
         }
 
-        private void OnTakeFocus()
+        private void SetFocus(bool focus)
         {
-            gotFocus = false;
+            if (webView == null) { return; }
 
-            if (parentField != null && parentRepaintMethod != null)
+            if (focus)
             {
-                var parent = parentField.GetValue(this);
-
-                parentRepaintMethod.Invoke(parent, null);
+                SetHostView();
+                ShowWebView();
+            }
+            else
+            {
+                DetachHostView();
+                HideWebView();
             }
 
-            if (webView != null)
-            {
-                webView.Detach();
-            }
+            Invoke(WebViewMethodType.SetFocus, focus);
         }
 
-        private void TrackFocusState()
+        private void DetachHostView()
         {
-            if (!gotFocus) { return; }
+            Invoke(WebViewMethodType.SetHostView, null);
+        }
 
-            if (this != focusedWindow)
+        private void SetHostView()
+        {
+            var parent = parentField.GetValue(this);
+
+            Invoke(WebViewMethodType.SetHostView, parent);
+            Refresh();
+            SetApplicationFocus(true);
+        }
+
+        private void SetSizeAndPosition(Rect position)
+        {
+            Invoke(WebViewMethodType.SetSizeAndPosition, (int)position.x, (int)position.y, (int)position.width, (int)position.height);
+        }
+
+        private void ShowWebView()
+        {
+            Invoke(WebViewMethodType.Show);
+        }
+
+        private void HideWebView()
+        {
+            Invoke(WebViewMethodType.Hide);
+        }
+
+        public void Forward()
+        {
+            Invoke(WebViewMethodType.Forward);
+        }
+
+        public void Back()
+        {
+            Invoke(WebViewMethodType.Back);
+        }
+
+        public void Reload()
+        {
+            Invoke(WebViewMethodType.Reload);
+        }
+
+        public bool HasApplicationFocus()
+        {
+            var method = webViewMethods.GetValueOrDefault(WebViewMethodType.HasApplicationFocus);
+
+            return (bool)method.Invoke(webView, null);
+        }
+
+        public void SetApplicationFocus(bool focus)
+        {
+            Invoke(WebViewMethodType.SetApplicationFocus, focus);
+        }
+
+        public void LoadURL(string url)
+        {
+            this.url = url;
+
+            Invoke(WebViewMethodType.LoadURL, url);
+        }
+
+        public void LoadHTML(string html)
+        {
+            Invoke(WebViewMethodType.LoadURL, "data:text/html;charset=utf-8," + html);
+        }
+
+        public void LoadFile(string path)
+        {
+            Invoke(WebViewMethodType.LoadURL, "file:///" + path);
+        }
+
+        public void ExecuteJavascript(string js)
+        {
+            Invoke(WebViewMethodType.ExecuteJavascript, js);
+        }
+
+        private void Invoke(WebViewMethodType methodType, params object[] args)
+        {
+            try
             {
-                OnTakeFocus();
+                var method = webViewMethods.GetValueOrDefault(methodType);
+
+                if (method == null)
+                {
+                    Debug.LogErrorFormat("Method not found. [{0}]", methodType.ToString());
+                }
+
+                method.Invoke(webView, args);
+            }
+            catch
+            {
+                // ignored
             }
         }
+
+        public IObservable<string> OnLocationChangedAsObservable()
+        {
+            return onLocationChanged ?? (onLocationChanged = new Subject<string>());
+        }
+
+        public IObservable<string> OnLoadErrorAsObservable()
+        {
+            return onLoadError ?? (onLoadError = new Subject<string>());
+        }
+
+        public IObservable<Unit> OnInitScriptingAsObservable()
+        {
+            return onInitScripting ?? (onInitScripting = new Subject<Unit>());
+        }
+
+        /* Default bindings for SetDelegateObject */
+
+        protected virtual void OnLocationChanged(string url)
+        {
+            if (url.StartsWith("http://") || url.StartsWith("https://"))
+            {
+                this.url = url;
+
+                if (onLocationChanged != null)
+                {
+                    onLocationChanged.OnNext(url);
+                }
+            }
+        }
+
+        protected virtual void OnLoadError(string url)
+        {
+            if (onLoadError != null)
+            {
+                onLoadError.OnNext(url);
+            }
+            else
+            {
+                Debug.LogError("WebView has failed to load " + url);
+            }
+        }
+
+        protected virtual void OnInitScripting()
+        {
+            if (onInitScripting != null)
+            {
+                onInitScripting.OnNext(Unit.Default);
+            }
+        }
+
+        protected virtual void OnOpenExternalLink(string url)
+        {
+            if (url.StartsWith("http://") || url.StartsWith("https://"))
+            {
+                Application.OpenURL(url);
+            }
+        }
+
+        protected virtual void OnWebViewDirty()
+        {
+            // This binding may not work
+        }
+
+        protected virtual void OnDownloadProgress(string id, string message, ulong bytes, ulong total)
+        {
+            // This binding may not work
+        }
+
+        protected virtual void OnBatchMode()
+        {
+            // This binding may not work
+        }
+
+        protected virtual void OnReceiveTitle(string title)
+        {
+            // This binding may not work
+        }
+
+        protected virtual void OnDomainReload()
+        {
+            // This binding may not work
+        }
+
+        #endregion
     }
 }
