6677071867aefb705364e4344c1735ba8a717201
oTAMAKOo <oTAMAKOo@hotmail.co.jp>
2019-09-27 03:40:14 +0900

    テキストカラータグ機能追加

---
+++ Scripts/Modules/UI/TextColorTag/Editor/TextColorTagSettingInspector.cs
@@ -0,0 +1,136 @@
+﻿
+using UnityEngine;
+using UnityEditor;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using Extensions;
+using Extensions.Devkit;
+
+namespace Modules.UI.TextColorTag
+{
+    [CustomEditor(typeof(TextColorTagSetting))]
+    public sealed class TextColorTagSettingInspector : Editor
+    {
+        //----- params -----
+               
+        //----- field -----
+
+        private Vector2 scrollPosition = Vector2.zero;
+
+        //----- property -----
+
+        //----- method -----
+
+        public override void OnInspectorGUI()
+        {
+            var instance = target as TextColorTagSetting;
+
+            var colorTagInfos = instance.GeTextColorTagInfos();
+
+            EditorGUILayout.Separator();
+
+            var headerItems = new List<EditorLayoutTools.ColumnHeaderContent>();
+
+            headerItems.Add(new EditorLayoutTools.ColumnHeaderContent("Color", GUILayout.Width(65f)));
+            headerItems.Add(new EditorLayoutTools.ColumnHeaderContent("Tag", GUILayout.MinWidth(150f)));
+
+            using (new EditorGUILayout.HorizontalScope())
+            {
+                GUILayout.Space(5f);
+
+                EditorLayoutTools.DrawColumnHeader(headerItems.ToArray());
+
+                GUILayout.Space(35f);
+
+                if (16 < colorTagInfos.Length)
+                {
+                    GUILayout.Space(18f);
+                }
+            }
+
+            var list = colorTagInfos.ToList();
+
+            var updated = false;
+            var deleteIndexs = new List<int>();
+
+            using (new ContentsScope())
+            {
+                using (var scrollViewScope = new EditorGUILayout.ScrollViewScope(scrollPosition, GUILayout.Height(300f)))
+                {
+                    for (var i = 0; i < list.Count; i++)
+                    {
+                        var result = DrawTextColorTagInfoGUI(list[i]);
+
+                        if (result == 1)
+                        {
+                            updated = true;
+                        }
+
+                        if (result == -1)
+                        {
+                            deleteIndexs.Add(i);
+                            updated = true;
+                        }
+                    }
+
+                    foreach (var index in deleteIndexs)
+                    {
+                        list.RemoveAt(index);
+                    }
+
+                    scrollPosition = scrollViewScope.scrollPosition;
+                }
+            }
+
+            using (new EditorGUILayout.HorizontalScope())
+            {
+                GUILayout.FlexibleSpace();
+
+                if (GUILayout.Button("+", EditorStyles.miniButton, GUILayout.Width(60f)))
+                {
+                    var info = new TextColorTagInfo();
+
+                    list.Add(info);
+
+                    updated = true;
+                }
+            }
+
+            if (updated)
+            {
+                UnityEditorUtility.RegisterUndo("TextColorTagSettingInspector Undo", instance);
+                Reflection.SetPrivateField(instance, "colorTagInfos", list.ToArray());
+            }
+        }
+
+        private int DrawTextColorTagInfoGUI(TextColorTagInfo info)
+        {
+            var result = 0;
+
+            using (new EditorGUILayout.HorizontalScope())
+            {
+                EditorGUI.BeginChangeCheck();
+
+                var color = EditorGUILayout.ColorField(info.color, GUILayout.Width(65f));
+
+                var tag = EditorGUILayout.TextField(info.tag, GUILayout.ExpandWidth(true));
+
+                if (EditorGUI.EndChangeCheck())
+                {
+                    info.color = color;
+                    info.tag = tag;
+
+                    result = 1;
+                }
+
+                if (GUILayout.Button("-", EditorStyles.miniButton, GUILayout.Width(25f)))
+                {
+                    result = -1;
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Scripts/Modules/UI/TextColorTag/TextColorTag.cs
@@ -0,0 +1,81 @@
+﻿
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using UniRx;
+using Extensions;
+
+namespace Modules.UI.TextColorTag
+{
+    [ExecuteInEditMode]
+    [RequireComponent(typeof(Text))]
+    public sealed class TextColorTag : MonoBehaviour
+    {
+        //----- params -----
+
+        //----- field -----
+
+        [SerializeField]
+        private TextColorTagSetting setting = null;
+
+        private Text textComponent = null;
+
+        private string prevText = null;
+
+        //----- property -----
+
+        //----- method -----
+
+        private void OnEnable()
+        {
+            UpdateText();
+        }
+
+        void LateUpdate()
+        {
+            UpdateText();
+        }
+
+        public void UpdateText()
+        {
+            if (setting == null) { return; }
+
+            if (textComponent == null)
+            {
+                textComponent = UnityUtility.GetComponent<Text>(gameObject);
+            }
+
+            if (string.IsNullOrEmpty(textComponent.text)) { return; }
+
+            if (textComponent.text != prevText)
+            {
+                textComponent.text = ConvertColorTag(textComponent.text);
+
+                prevText = textComponent.text;
+
+                Debug.Log("update");
+            }
+        }
+
+        private string ConvertColorTag(string text)
+        {
+            var colorTaginfos = setting.GeTextColorTagInfos();
+
+            if (colorTaginfos.IsEmpty()) { return text; }
+
+            foreach (var colorTaginfo in colorTaginfos)
+            {
+                var from = string.Format("<c={0}>", colorTaginfo.tag);
+                var to = string.Format("<color=#{0}>", ColorUtility.ToHtmlStringRGBA(colorTaginfo.color));
+
+                text = text.Replace(from, to);
+            }
+
+            text = text.Replace("</c>", "</color>");
+
+            return text;
+        }
+    }
+}
