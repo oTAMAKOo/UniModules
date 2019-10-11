2606bf3fd90699a229b1e3ec9af1cb9fd5bfec83
竹内誠 <makoto.takeuchi@drecom.co.jp>
2019-10-11 11:17:02 +0900

    コメント追加

--- Scripts/Modules/Devkit/AssetTuning/Editor/Tuners/Texture/TextureAssetTunerConfig.cs
+++ Scripts/Modules/Devkit/AssetTuning/Editor/Tuners/Texture/TextureAssetTunerConfig.cs
@@ -13,7 +13,7 @@ namespace Modules.Devkit.AssetTuning
         //----- field -----
 
         // compress
-
+        
         [SerializeField]
         private Object[] compressFolders = null;
         [SerializeField]
@@ -30,29 +30,34 @@ namespace Modules.Devkit.AssetTuning
 
         //----- property -----
 
+        /// <summary> 圧縮設定を適用するフォルダ. </summary>
         public Object[] CompressFolders
         {
             get { return compressFolders ?? (compressFolders = new Object[0]); }
         }
 
+        /// <summary> 圧縮設定の適用から除外するフォルダ名. </summary>
         public string[] IgnoreCompressFolderNames
         {
             get { return ignoreCompressFolderNames ?? (ignoreCompressFolderNames = new string[0]); }
         }
 
+        /// <summary> TextureTypeをSpriteに設定するフォルダ. </summary>
         public Object[] SpriteFolders
         {
             get { return spriteFolders ?? (spriteFolders = new Object[0]); }
         }
 
-        public string[] IgnoreSpriteFolderNames
-        {
-            get { return ignoreSpriteFolderNames ?? (ignoreSpriteFolderNames = new string[0]); }
-        }
-
+        /// <summary> TextureTypeをSpriteに設定するフォルダ名. </summary>
         public string[] SpriteFolderNames
         {
             get { return spriteFolderNames ?? (spriteFolderNames = new string[0]); }
+        }
+
+        /// <summary> TextureTypeをSpriteに設定適用から除外するフォルダ名. </summary>
+        public string[] IgnoreSpriteFolderNames
+        {
+            get { return ignoreSpriteFolderNames ?? (ignoreSpriteFolderNames = new string[0]); }
         }
 
         //----- method -----
