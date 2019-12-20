91dde3b1ebf498d604463277059f8b8657021bf4
oTAMAKOo <oTAMAKOo@hotmail.co.jp>
2019-12-21 00:05:09 +0900

    インデントレベルスコープ追加

---
+++ Scripts/Extensions/Devkit/Editor/GUIScope/IndentLevelScope.cs
@@ -0,0 +1,31 @@
+﻿
+using UnityEngine;
+using UnityEditor;
+
+namespace Extensions.Devkit
+{
+    public sealed class IndentLevelScope : GUI.Scope
+    {
+        //----- params -----
+
+        //----- field -----
+
+        private int originIndentLevel = 0;
+
+        //----- property -----
+
+        //----- method -----
+
+        public IndentLevelScope(int indentLevel)
+        {
+            originIndentLevel = EditorGUI.indentLevel;
+
+            EditorGUI.indentLevel = indentLevel;
+        }
+
+        protected override void CloseScope()
+        {
+            EditorGUI.indentLevel = originIndentLevel;
+        }
+    }
+}
