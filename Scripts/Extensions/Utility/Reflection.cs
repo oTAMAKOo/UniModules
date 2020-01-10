3d4c5b7584a5a8a0762190bd13a76ef644971da5
竹内誠 <makoto.takeuchi@drecom.co.jp>
2020-01-10 14:26:27 +0900

    リフレクション拡張機能追加・変数名変更

--- Scripts/Extensions/Utility/Reflection.cs
+++ Scripts/Extensions/Utility/Reflection.cs
@@ -7,106 +7,106 @@ namespace Extensions
     public static class Reflection
     {
         /// <summary> Public属性の変数の値を取得 </summary>
-        public static TResult GetPublicField<T, TResult>(T instance, string fieldName, BindingFlags? options = null)
+        public static TResult GetPublicField<T, TResult>(T instance, string fieldName, BindingFlags? bindingFlags = null)
         {
             var flags = BindingFlags.GetField | BindingFlags.Public | BindingFlags.Instance;
 
-            if (options.HasValue)
+            if (bindingFlags.HasValue)
             {
-                flags |= options.Value;
+                flags |= bindingFlags.Value;
             }
 
-            var fieldInfo = typeof(T).GetField(fieldName, flags);
+            var fieldInfo = GetFieldInfo(typeof(T), fieldName, flags);
 
             return (TResult)fieldInfo.GetValue(instance);
         }
 
         /// <summary> Private属性の変数の値を取得 </summary>
-        public static TResult GetPrivateField<T, TResult>(T instance, string fieldName, BindingFlags? options = null)
+        public static TResult GetPrivateField<T, TResult>(T instance, string fieldName, BindingFlags? bindingFlags = null)
         {
             var flags = BindingFlags.GetField | BindingFlags.NonPublic | BindingFlags.Instance;
 
-            if (options.HasValue)
+            if (bindingFlags.HasValue)
             {
-                flags |= options.Value;
+                flags |= bindingFlags.Value;
             }
 
-            var fieldInfo = typeof(T).GetField(fieldName, flags);
+            var fieldInfo = GetFieldInfo(typeof(T), fieldName, flags);
 
             return (TResult)fieldInfo.GetValue(instance);
         }
 
         /// <summary> Public属性の変数に値を設定 </summary>
-        public static void SetPublicField<T, TValue>(T instance, string fieldName, TValue value, BindingFlags? options = null)
+        public static void SetPublicField<T, TValue>(T instance, string fieldName, TValue value, BindingFlags? bindingFlags = null)
         {
             var flags = BindingFlags.GetField | BindingFlags.SetField | BindingFlags.Public | BindingFlags.Instance;
 
-            if (options.HasValue)
+            if (bindingFlags.HasValue)
             {
-                flags |= options.Value;
+                flags |= bindingFlags.Value;
             }
 
-            var fieldInfo = typeof(T).GetField(fieldName, flags);
+            var fieldInfo = GetFieldInfo(typeof(T), fieldName, flags);
 
             fieldInfo.SetValue(instance, value);
         }
 
         /// <summary> Private属性の変数に値を設定 </summary>
-        public static void SetPrivateField<T, TValue>(T instance, string fieldName, TValue value, BindingFlags? options = null)
+        public static void SetPrivateField<T, TValue>(T instance, string fieldName, TValue value, BindingFlags? bindingFlags = null)
         {
             var flags = BindingFlags.GetField | BindingFlags.SetField | BindingFlags.NonPublic | BindingFlags.Instance;
 
-            if (options.HasValue)
+            if (bindingFlags.HasValue)
             {
-                flags |= options.Value;
+                flags |= bindingFlags.Value;
             }
 
-            var fieldInfo = typeof(T).GetField(fieldName, flags);
+            var fieldInfo = GetFieldInfo(typeof(T), fieldName, flags);
 
             fieldInfo.SetValue(instance, value);
         }
 
         /// <summary> Private属性のプロパティの値を取得 </summary>
-        public static TResult GetPrivateProperty<T, TResult>(T instance, string propertyName, object[] index = null, BindingFlags? options = null)
+        public static TResult GetPrivateProperty<T, TResult>(T instance, string propertyName, object[] index = null, BindingFlags? bindingFlags = null)
         {
             var flags = BindingFlags.GetProperty | BindingFlags.NonPublic | BindingFlags.Instance;
 
-            if (options.HasValue)
+            if (bindingFlags.HasValue)
             {
-                flags |= options.Value;
+                flags |= bindingFlags.Value;
             }
 
-            var propertyInfo = typeof(T).GetProperty(propertyName, flags);
+            var propertyInfo = GetPropertyInfo(typeof(T), propertyName, flags);
 
             return (TResult)propertyInfo.GetValue(instance, index);
         }
 
         /// <summary> Public属性のプロパティの値を取得 </summary>
-        public static TResult GetPublicProperty<T, TResult>(T instance, string propertyName, object[] index = null, BindingFlags? options = null)
+        public static TResult GetPublicProperty<T, TResult>(T instance, string propertyName, object[] index = null, BindingFlags? bindingFlags = null)
         {
             var flags = BindingFlags.GetProperty | BindingFlags.Public | BindingFlags.Instance;
 
-            if (options.HasValue)
+            if (bindingFlags.HasValue)
             {
-                flags |= options.Value;
+                flags |= bindingFlags.Value;
             }
 
-            var propertyInfo = typeof(T).GetProperty(propertyName, flags);
+            var propertyInfo = GetPropertyInfo(typeof(T), propertyName, flags);
 
             return (TResult)propertyInfo.GetValue(instance, index);
         }
 
         /// <summary> Private属性のプロパティに値を設定 </summary>
-        public static void SetPrivateProperty<T, TValue>(T instance, string propertyName, TValue value, object[] index = null, BindingFlags? options = null)
+        public static void SetPrivateProperty<T, TValue>(T instance, string propertyName, TValue value, object[] index = null, BindingFlags? bindingFlags = null)
         {
             var flags = BindingFlags.GetProperty | BindingFlags.SetField | BindingFlags.NonPublic | BindingFlags.Instance;
 
-            if (options.HasValue)
+            if (bindingFlags.HasValue)
             {
-                flags |= options.Value;
+                flags |= bindingFlags.Value;
             }
 
-            var propertyInfo = typeof(T).GetProperty(propertyName, flags);
+            var propertyInfo = GetPropertyInfo(typeof(T), propertyName, flags);
 
             if (propertyInfo.CanWrite)
             {
@@ -121,7 +121,9 @@ namespace Extensions
         /// <summary> Public属性のプロパティに値を設定 </summary>
         public static void SetPublicProperty<T, TValue>(T instance, string propertyName, TValue value, object[] index = null)
         {
-            var propertyInfo = typeof(T).GetProperty(propertyName);
+            var flags = BindingFlags.Public | BindingFlags.Instance;
+
+            var propertyInfo = GetPropertyInfo(typeof(T), propertyName, flags);
 
             if (propertyInfo.CanWrite)
             {
@@ -134,33 +136,78 @@ namespace Extensions
         }
 
         /// <summary> Private属性の関数を実行 </summary>
-        public static object InvokePrivateMethod<T>(T instance, string methodName, object[] parameters = null, BindingFlags? options = null)
+        public static object InvokePrivateMethod<T>(T instance, string methodName, object[] parameters = null, BindingFlags? bindingFlags = null)
         {
             var flags = BindingFlags.NonPublic | BindingFlags.Instance;
 
-            if (options.HasValue)
+            if (bindingFlags.HasValue)
             {
-                flags |= options.Value;
+                flags |= bindingFlags.Value;
             }
 
-            var methodInfo = typeof(T).GetMethod(methodName, flags);
+            var methodInfo = GetMethodInfo(typeof(T), methodName, flags);
 
             return methodInfo.Invoke(instance, parameters);
         }
 
         /// <summary> Public属性の関数を実行 </summary>
-        public static object InvokePublicMethod<T>(T instance, string methodName, object[] parameters = null, BindingFlags? options = null)
+        public static object InvokePublicMethod<T>(T instance, string methodName, object[] parameters = null, BindingFlags? bindingFlags = null)
         {
             var flags = BindingFlags.Public | BindingFlags.Instance;
 
-            if (options.HasValue)
+            if (bindingFlags.HasValue)
             {
-                flags |= options.Value;
+                flags |= bindingFlags.Value;
             }
 
-            var methodInfo = typeof(T).GetMethod(methodName, flags);
+            var methodInfo = GetMethodInfo(typeof(T), methodName, flags);
 
             return methodInfo.Invoke(instance, parameters);
+        }
+
+        /// <summary> 変数情報を取得 </summary>
+        public static FieldInfo GetFieldInfo(Type type, string fieldName, BindingFlags bindingFlags)
+        {
+            var info = type.GetField(fieldName, bindingFlags);
+
+            if (info != null) { return info; }
+
+            if (type.BaseType != null)
+            {
+                return GetFieldInfo(type.BaseType, fieldName, bindingFlags);
+            }
+
+            return null;
+        }
+
+        /// <summary> プロパティ情報を取得 </summary>
+        public static PropertyInfo GetPropertyInfo(Type type, string propertyName, BindingFlags bindingFlags)
+        {
+            var info = type.GetProperty(propertyName, bindingFlags);
+
+            if (info != null) { return info; }
+
+            if (type.BaseType != null)
+            {
+                return GetPropertyInfo(type.BaseType, propertyName, bindingFlags);
+            }
+
+            return null;
+        }
+
+        /// <summary> 関数情報を取得 </summary>
+        public static MethodInfo GetMethodInfo(Type type, string methodName, BindingFlags bindingFlags)
+        {
+            var info = type.GetMethod(methodName, bindingFlags);
+
+            if (info != null) { return info; }
+
+            if (type.BaseType != null)
+            {
+                return GetMethodInfo(type.BaseType, methodName, bindingFlags);
+            }
+
+            return null;
         }
     }
 }
