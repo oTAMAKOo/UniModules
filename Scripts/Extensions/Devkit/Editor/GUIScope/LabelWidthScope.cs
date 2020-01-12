4c329d676eb9eed4047ceee22a976c47669fe21a
oTAMAKOo <oTAMAKOo@hotmail.co.jp>
2020-01-12 13:40:01 +0900

    ラベル幅スコープ追加

---
+++ Scripts/Extensions/Devkit/Editor/GUIScope/LabelWidthScope.cs
@@ -0,0 +1,31 @@
+﻿
+using UnityEngine;
+using UnityEditor;
+
+namespace Extensions.Devkit
+{
+    public class LabelWidthScope : GUI.Scope
+    {
+        //----- params -----
+
+        //----- field -----
+
+        private readonly float originLabelWidth = 0f;
+
+        //----- property -----
+
+        //----- method -----
+
+        public LabelWidthScope(float labelWidth)
+        {
+            originLabelWidth = EditorGUIUtility.labelWidth;
+
+            EditorGUIUtility.labelWidth = labelWidth;
+        }
+
+        protected override void CloseScope()
+        {
+            EditorGUIUtility.labelWidth = originLabelWidth;
+        }
+    }
+}
