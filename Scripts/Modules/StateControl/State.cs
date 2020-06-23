e95c1cdf2b82ecb0f372c0546d907ee7101c5382
oTAMAKOo <oTAMAKOo@hotmail.co.jp>
2020-06-24 02:30:44 +0900

    State管理機能実装

---
+++ Scripts/Modules/StateControl/State.cs
@@ -0,0 +1,167 @@
+﻿
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UniRx;
+using Extensions;
+
+namespace Modules.StateControl
+{
+    public abstract class StateArgument { }
+
+    public interface IState<T> where T : Enum
+    {
+        T Type { get; }
+
+        IEnumerator Enter();
+
+        IEnumerator Exit();
+    }
+
+    public abstract class State<T> : IState<T> where T : Enum
+    {
+        //----- params -----
+
+        //----- field -----
+
+        private SortedDictionary<int, List<Func<IEnumerator>>> enterFunctions = null;
+
+        private SortedDictionary<int, List<Func<IEnumerator>>> exitFunctions = null;
+
+        //----- property -----
+
+        public abstract T Type { get; }
+
+        //----- method -----
+
+        public State()
+        {
+            enterFunctions = new SortedDictionary<int, List<Func<IEnumerator>>>();
+            exitFunctions = new SortedDictionary<int, List<Func<IEnumerator>>>();
+        }
+
+        /// <summary> 開始イベント追加 </summary>
+        public void AddEnterFunction(Func<IEnumerator> function, int priority = 0)
+        {
+            var list = enterFunctions.GetOrAdd(priority, i => new List<Func<IEnumerator>>());
+
+            list.Add(function);
+        }
+
+        /// <summary> 終了イベント追加 </summary>
+        public void AddExitFunction(Func<IEnumerator> function, int priority = 0)
+        {
+            var list = exitFunctions.GetOrAdd(priority, i => new List<Func<IEnumerator>>());
+
+            list.Add(function);
+        }
+
+        /// <summary> 開始処理実行 </summary>
+        public IEnumerator Enter()
+        {
+            foreach (var item in enterFunctions.Values)
+            {
+                var enterYield = item.Select(x => Observable.FromCoroutine(x)).WhenAll().ToYieldInstruction();
+
+                while (!enterYield.IsDone)
+                {
+                    yield return null;
+                }
+            }
+        }
+
+        /// <summary> 終了処理実行 </summary>
+        public IEnumerator Exit()
+        {
+            foreach (var item in exitFunctions.Values)
+            {
+                var exitYield = item.Select(x => Observable.FromCoroutine(x)).WhenAll().ToYieldInstruction();
+
+                while (!exitYield.IsDone)
+                {
+                    yield return null;
+                }
+            }
+        }
+    }
+
+    public abstract class State<T, TArgument> : IState<T> where T : Enum where TArgument : StateArgument, new()
+    {
+        //----- params -----
+
+        //----- field -----
+
+        private SortedDictionary<int, List<Func<TArgument, IEnumerator>>> enterFunctions = null;
+
+        private SortedDictionary<int, List<Func<IEnumerator>>> exitFunctions = null;
+
+        private TArgument argument = null;
+
+        //----- property -----
+
+        public abstract T Type { get; }
+
+        //----- method -----
+
+        public State()
+        {
+            enterFunctions = new SortedDictionary<int, List<Func<TArgument, IEnumerator>>>();
+            exitFunctions = new SortedDictionary<int, List<Func<IEnumerator>>>();
+        }
+
+        public void SetArgument(StateArgument enterArgument)
+        {
+            argument = enterArgument as TArgument;
+
+            if (argument == null)
+            {
+                argument = new TArgument();
+            }
+        }
+
+        /// <summary> 開始イベント追加 </summary>
+        public void AddEnterFunction(Func<TArgument, IEnumerator> function, int priority = 0)
+        {
+            var list = enterFunctions.GetOrAdd(priority, i => new List<Func<TArgument, IEnumerator>>());
+
+            list.Add(function);
+        }
+
+        /// <summary> 終了イベント追加 </summary>
+        public void AddExitFunction(Func<IEnumerator> function, int priority = 0)
+        {
+            var list = exitFunctions.GetOrAdd(priority, i => new List<Func<IEnumerator>>());
+
+            list.Add(function);
+        }
+
+        /// <summary> 開始処理実行 </summary>
+        public IEnumerator Enter()
+        {
+            foreach (var item in enterFunctions.Values)
+            {
+                var enterYield = item.Select(x => Observable.FromCoroutine(() => x(argument))).WhenAll().ToYieldInstruction();
+
+                while (!enterYield.IsDone)
+                {
+                    yield return null;
+                }
+            }
+        }
+
+        /// <summary> 終了処理実行 </summary>
+        public IEnumerator Exit()
+        {
+            foreach (var item in exitFunctions.Values)
+            {
+                var exitYield = item.Select(x => Observable.FromCoroutine(x)).WhenAll().ToYieldInstruction();
+
+                while (!exitYield.IsDone)
+                {
+                    yield return null;
+                }
+            }
+        }
+    }
+}
---
+++ Scripts/Modules/StateControl/StateController.cs
@@ -0,0 +1,132 @@
+﻿
+using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UniRx;
+using Extensions;
+
+namespace Modules.StateControl
+{
+    public sealed class StateController<T> where T : Enum
+    {
+        //----- params -----
+
+        private sealed class StateEmptyArgument : StateArgument { }
+
+        //----- field -----
+
+        private LifetimeDisposable lifetimeDisposable = null;
+
+        private IState<T> currentState = null;
+
+        private Dictionary<T, IState<T>> stateTable = null;
+
+        private IDisposable changeStateDisposable = null;
+
+        //----- property -----
+
+        public T Current { get { return currentState != null ? currentState.Type : default; } }
+
+        //----- method -----
+
+        public StateController()
+        {
+            lifetimeDisposable = new LifetimeDisposable();
+
+            stateTable = new Dictionary<T, IState<T>>();
+        }
+
+        /// <summary> 登録済みのステートを取得 </summary>
+        public TState GetState<TState>() where TState : class, IState<T>
+        {
+            return stateTable.Values.FirstOrDefault(x => x is TState) as TState;
+        }
+
+        /// <summary> ステートを登録 </summary>
+        public void Register(IState<T> stateInstance)
+        {
+            var type = stateInstance.Type;
+
+            if (stateTable.ContainsKey(type))
+            {
+                throw new ArgumentException(string.Format("This state is already registered: {0}", type));
+            }
+
+            stateTable[type] = stateInstance;
+        }
+
+        /// <summary> 登録されたステートをクリア </summary>
+        public void Reset()
+        {
+            if (changeStateDisposable != null)
+            {
+                changeStateDisposable.Dispose();
+                changeStateDisposable = null;
+            }
+
+            stateTable.Clear();
+
+            currentState = null;
+        }
+
+        /// <summary> ステート変更を要求 </summary>
+        public void Request(T next)
+        {
+            Request(next, new StateEmptyArgument());
+        }
+
+        /// <summary> ステート変更を要求 </summary>
+        public void Request<TArgument>(T next, TArgument argument) where TArgument : StateArgument, new()
+        {
+            if (changeStateDisposable != null)
+            {
+                changeStateDisposable.Dispose();
+                changeStateDisposable = null;
+            }
+
+            changeStateDisposable = Observable.FromCoroutine(() => ChangeState(next, argument))
+                .Subscribe(_ => changeStateDisposable = null)
+                .AddTo(lifetimeDisposable.Disposable);
+        }
+
+        private IEnumerator ChangeState<TArgument>(T next, TArgument argument) where TArgument : StateArgument, new()
+        {
+            var prevState = currentState;
+
+            var nextState = stateTable.GetValueOrDefault(next);
+
+            // 前のステートの終了待ち.
+
+            if (prevState != null)
+            {
+                var exitYield = prevState.Exit().ToYieldInstruction();
+
+                while (!exitYield.IsDone)
+                {
+                    yield return null;
+                }
+            }
+
+            currentState = nextState ?? throw new ArgumentException(string.Format("The specified state is not registered. State: {0}", next));
+
+            // 引数を設定.
+
+            var stateClass = currentState as State<T, TArgument>;
+
+            if (stateClass != null)
+            {
+                stateClass.SetArgument(argument);
+            }
+
+            // ステートの開始.
+
+            var enterYield = currentState.Enter().ToYieldInstruction();
+
+            while (!enterYield.IsDone)
+            {
+                yield return null;
+            }
+        }
+    }
+}
