e5a276fcbc63a2e0286c0b5859e67530659fb548
oTAMAKOo <oTAMAKOo@hotmail.co.jp>
2020-11-29 12:40:24 +0900

    不要な定義削除

--- Scripts/Modules/CriWare/CriAssetDefinition.cs
+++ Scripts/Modules/CriWare/CriAssetDefinition.cs
@@ -1,21 +1,10 @@
 ﻿
 #if ENABLE_CRIWARE_ADX || ENABLE_CRIWARE_SOFDEC
-﻿﻿
-using UnityEngine;
-using Unity.Linq;
-using System;
-using System.Linq;
-using System.Collections.Generic;
-using System.IO;
-using UniRx;
-using Extensions;
 
 namespace Modules.CriWare
 {
     public static class CriAssetDefinition
 	{
-        public const string VersionFileExtension = ".ver";
-
         public const string AcfExtension = ".acf";
         public const string AcbExtension = ".acb";
         public const string AwbExtension = ".awb";
