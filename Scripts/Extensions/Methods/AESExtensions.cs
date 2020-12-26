7ffb91da0e32ffc06af984d52dcbcab56c9b973e
oTAMAKOo <oTAMAKOo@hotmail.co.jp>
2020-12-26 11:38:11 +0900

    CreateDecryptor、CreateEncryptorがスレッドだと使えないので毎回作る形式に戻す

--- Scripts/Extensions/Methods/AESExtensions.cs
+++ Scripts/Extensions/Methods/AESExtensions.cs
@@ -17,16 +17,17 @@ namespace Extensions
 
             byte[] result = null;
 
-            var encryptor = aesCryptoKey.Encryptor;
-
-            using (var memoryStream = new MemoryStream())
+            using (var encryptor = aesCryptoKey.AesManaged.CreateEncryptor())
             {
-                using (var cryptoStream = new CryptoStream(memoryStream, encryptor, CryptoStreamMode.Write))
+                using (var memoryStream = new MemoryStream())
                 {
-                    cryptoStream.Write(value, 0, value.Length);
-                    cryptoStream.Close();
+                    using (var cryptoStream = new CryptoStream(memoryStream, encryptor, CryptoStreamMode.Write))
+                    {
+                        cryptoStream.Write(value, 0, value.Length);
+                        cryptoStream.Close();
 
-                    result = memoryStream.ToArray();
+                        result = memoryStream.ToArray();
+                    }
                 }
             }
 
@@ -42,19 +43,20 @@ namespace Extensions
 
             byte[] result = null;
 
-            var decryptor = aesCryptoKey.Decryptor;
-
-            using (var memoryStream = new MemoryStream())
+            using (var decryptor = aesCryptoKey.AesManaged.CreateDecryptor())
             {
-                using (var cryptoStream = new CryptoStream(memoryStream, decryptor, CryptoStreamMode.Write))
+                using (var memoryStream = new MemoryStream())
                 {
-                    cryptoStream.Write(value, 0, value.Length);
-                    cryptoStream.Close();
+                    using (var cryptoStream = new CryptoStream(memoryStream, decryptor, CryptoStreamMode.Write))
+                    {
+                        cryptoStream.Write(value, 0, value.Length);
+                        cryptoStream.Close();
 
-                    result = memoryStream.ToArray();
+                        result = memoryStream.ToArray();
+                    }
                 }
             }
-
+            
             return result;
         }
 
@@ -66,24 +68,25 @@ namespace Extensions
             if (string.IsNullOrEmpty(value)) { return null; }
 
             string result = null;
-
-            var encryptor = aesCryptoKey.Encryptor;
             
-            using (var memoryStream = new MemoryStream())
+            using (var encryptor = aesCryptoKey.AesManaged.CreateEncryptor())
             {
-                using (var cryptoStream = new CryptoStream(memoryStream, encryptor, CryptoStreamMode.Write))
+                using (var memoryStream = new MemoryStream())
                 {
-                    var toEncrypt = Encoding.UTF8.GetBytes(value);
+                    using (var cryptoStream = new CryptoStream(memoryStream, encryptor, CryptoStreamMode.Write))
+                    {
+                        var toEncrypt = Encoding.UTF8.GetBytes(value);
 
-                    cryptoStream.Write(toEncrypt, 0, toEncrypt.Length);
-                    cryptoStream.FlushFinalBlock();
+                        cryptoStream.Write(toEncrypt, 0, toEncrypt.Length);
+                        cryptoStream.FlushFinalBlock();
 
-                    var encrypted = memoryStream.ToArray();
+                        var encrypted = memoryStream.ToArray();
 
-                    result = Convert.ToBase64String(encrypted).Replace('+', '-').Replace('/', '_');
+                        result = Convert.ToBase64String(encrypted).Replace('+', '-').Replace('/', '_');
+                    }
                 }
             }
-
+            
             return result;
         }
 
@@ -95,19 +98,20 @@ namespace Extensions
             if (string.IsNullOrEmpty(value)) { return null; }
 
             string result = null;
-
-            var decryptor = aesCryptoKey.Decryptor;
-
+            
             var encrypted = Convert.FromBase64String(value.Replace('-', '+').Replace('_', '/'));
             var fromEncrypt = new byte[encrypted.Length];
 
-            using (var memoryStream = new MemoryStream(encrypted))
+            using (var decryptor = aesCryptoKey.AesManaged.CreateDecryptor())
             {
-                using (var cryptoStream = new CryptoStream(memoryStream, decryptor, CryptoStreamMode.Read))
+                using (var memoryStream = new MemoryStream(encrypted))
                 {
-                    cryptoStream.Read(fromEncrypt, 0, fromEncrypt.Length);
+                    using (var cryptoStream = new CryptoStream(memoryStream, decryptor, CryptoStreamMode.Read))
+                    {
+                        cryptoStream.Read(fromEncrypt, 0, fromEncrypt.Length);
 
-                    result = Encoding.UTF8.GetString(fromEncrypt);
+                        result = Encoding.UTF8.GetString(fromEncrypt);
+                    }
                 }
             }
 
@@ -116,85 +120,21 @@ namespace Extensions
         }
     }
 
-    public sealed class AesCryptoKey : IDisposable
+    public sealed class AesCryptoKey
     {
         //----- params -----
         
         private static readonly byte[] Salt = { 0xe6, 0xdc, 0xff, 0x74, 0xad, 0xad, 0x7a, 0xee, 0xc5, 0xfe, 0x50, 0xaf, 0x4d, 0x08, 0x2d, 0x3c };
 
         //----- field -----
-
-        private ICryptoTransform encryptor = null;
-
-        private ICryptoTransform decryptor = null;
-
+        
         //----- property -----
 
         public AesManaged AesManaged { get; private set; }
 
-        public ICryptoTransform Encryptor
-        {
-            get { return encryptor ?? (encryptor = AesManaged.CreateEncryptor()); }
-        }
-
-        public ICryptoTransform Decryptor
-        {
-            get { return decryptor ?? (decryptor = AesManaged.CreateDecryptor()); }
-        }
-
         //----- method -----
 
         public AesCryptoKey(string password)
-        {
-            AesManaged = CreateAesManaged(password);
-        }
-
-        public AesCryptoKey(string key, string iv)
-        {
-            AesManaged = CreateAesManaged(key, iv);
-        }
-
-        ~AesCryptoKey()
-        {
-            Dispose();
-        }
-
-        public void Dispose()
-        {
-            if (encryptor != null)
-            {
-                encryptor.Dispose();
-            }
-
-            if (decryptor != null)
-            {
-                decryptor.Dispose();
-            }
-
-            if (AesManaged != null)
-            {
-                AesManaged.Dispose();
-            }
-
-            GC.SuppressFinalize(this);
-        }
-
-        private static AesManaged CreateDefaultAesManaged()
-        {
-            var aesManaged = new AesManaged()
-            {
-                BlockSize = 128,
-                Padding = PaddingMode.PKCS7,
-                Mode = CipherMode.CBC,
-            };
-
-            return aesManaged;
-        }
-
-        /// <summary>
-        /// パスワードを指定してAesManagedを生成.
-        /// </summary>
-        private static AesManaged CreateAesManaged(string password)
         {
             if (string.IsNullOrEmpty(password))
             {
@@ -204,28 +144,33 @@ namespace Extensions
             // 疑似乱数を使用してパスワードを暗号化.
             var pdb = new Rfc2898DeriveBytes(password, Salt, 64);
 
-            var aesManaged = CreateDefaultAesManaged();
+            AesManaged = CreateAesManaged();
 
-            aesManaged.Key = pdb.GetBytes(32);
-            aesManaged.IV = pdb.GetBytes(16);
-
-            return aesManaged;
+            AesManaged.Key = pdb.GetBytes(32);
+            AesManaged.IV = pdb.GetBytes(16);
         }
 
-        /// <summary>
-        /// キー、ベクトルを指定してAesManagedを生成.
-        /// </summary>
-        private static AesManaged CreateAesManaged(string key, string iv)
+        public AesCryptoKey(string key, string iv)
         {
             if (string.IsNullOrEmpty(key) || string.IsNullOrEmpty(iv))
             {
                 throw new ArgumentException("Require key and iv string.");
             }
 
-            var aesManaged = CreateDefaultAesManaged();
+            AesManaged = CreateAesManaged();
 
-            aesManaged.Key = Encoding.UTF8.GetBytes(key);
-            aesManaged.IV = Encoding.UTF8.GetBytes(iv);
+            AesManaged.Key = Encoding.UTF8.GetBytes(key);
+            AesManaged.IV = Encoding.UTF8.GetBytes(iv);
+        }
+        
+        private static AesManaged CreateAesManaged()
+        {
+            var aesManaged = new AesManaged()
+            {
+                BlockSize = 128,
+                Padding = PaddingMode.PKCS7,
+                Mode = CipherMode.CBC,
+            };
 
             return aesManaged;
         }
