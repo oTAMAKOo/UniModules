f866e4d896bd34ca25e45b938c34f71aeae1170a
oTAMAKOo <oTAMAKOo@hotmail.co.jp>
2021-05-25 03:23:48 +0900

    ビルド時削除対象登録実装

---
+++ Scripts/Modules/Devkit/Build/Editor/DeleteAsset/DeleteAssetSetting.cs
@@ -0,0 +1,24 @@
+﻿
+using UnityEngine;
+using UnityEditor;
+using System.Collections.Generic;
+using System.Linq;
+using Modules.Devkit.ScriptableObjects;
+
+namespace Modules.Devkit.Build
+{
+    public sealed class DeleteAssetSetting : ReloadableScriptableObject<DeleteAssetSetting>
+    {
+        [SerializeField]
+        private string[] targetGuids = null;
+
+        public IReadOnlyCollection<Object> GetTargets()
+        {
+            return targetGuids.Where(x => !string.IsNullOrEmpty(x))
+                .Select(x => AssetDatabase.GUIDToAssetPath(x))
+                .Where(x => !string.IsNullOrEmpty(x))
+                .Select(x => AssetDatabase.LoadAssetAtPath(x, typeof(Object)))
+                .ToArray();
+        }
+    }
+}
---
+++ Scripts/Modules/Devkit/Build/Editor/DeleteAsset/DeleteAssetSettingInspector.cs
@@ -0,0 +1,51 @@
+﻿
+using UnityEditor;
+using System.Linq;
+using UniRx;
+using Extensions;
+using Extensions.Devkit;
+using Modules.Devkit.Inspector;
+
+namespace Modules.Devkit.Build
+{
+    [CustomEditor(typeof(DeleteAssetSetting))]
+    public sealed class DeleteAssetSettingInspector : UnityEditor.Editor
+    {
+        private AssetRegisterScrollView assetListView = null;
+
+        private DeleteAssetSetting instance = null;
+
+        void OnEnable()
+        {
+            instance = target as DeleteAssetSetting;
+
+            var guids = Reflection.GetPrivateField<DeleteAssetSetting, string[]>(instance, "targetGuids");
+
+            assetListView = new AssetRegisterScrollView("Delete Assets", "DeleteAssetSettingInspector-Delete Assets");
+            assetListView.RemoveChildrenAssets = true;
+            assetListView.SetContents(guids);
+
+            assetListView.OnUpdateContentsAsObservable().Subscribe(x => OnUpdateContents(x));
+        }
+
+        public override void OnInspectorGUI()
+        {
+            instance = target as DeleteAssetSetting;
+
+            assetListView.DrawGUI();
+        }
+
+        private void OnUpdateContents(AssetRegisterScrollView.AssetInfo[] infos)
+        {
+            var guids = infos.Select(x => x.guid)
+                .Where(x => !string.IsNullOrEmpty(x))
+                .ToArray();
+
+            UnityEditorUtility.RegisterUndo("DeleteAssetSettingInspector Undo", instance);
+
+            Reflection.SetPrivateField<DeleteAssetSetting, string[]>(instance, "targetGuids", guids);
+
+            UnityEditorUtility.SaveAsset(instance);
+        }
+    }
+}
