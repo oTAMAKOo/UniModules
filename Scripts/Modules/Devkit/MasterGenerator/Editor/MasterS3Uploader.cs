7d124018654bcf4a6e2def0d731b6520ad54b2f1
ma-takeuchi <ma-takeuchi@nextninja.co.jp>
2022-04-27 15:10:50 +0900

    アップロード周り修正

---
+++ Scripts/Modules/Devkit/MasterGenerator/Editor/MasterS3Uploader.cs
@@ -0,0 +1,80 @@
+
+using UnityEngine;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Extensions;
+using Modules.Master.Editor;
+
+namespace Modules.Master
+{
+    public sealed class MasterS3Uploader
+    {
+        //----- params -----
+
+        //----- field -----
+
+        //----- property -----
+
+        //----- method -----
+
+        public static async Task<bool> Upload(S3Uploader uploader)
+        {
+	        var exportPath = MasterGenerator.GetExportDirectory();
+
+	        if (string.IsNullOrEmpty(exportPath))
+	        {
+		        Debug.LogError("Export path is empty.");
+
+		        return false;
+	        }
+
+			// バージョンファイルから作成時のコミットハッシュを取得.
+
+	        var versionFilePath = PathUtility.Combine(exportPath, MasterGenerator.VersionFileName);
+
+	        if (!File.Exists(versionFilePath))
+	        {
+		        Debug.LogErrorFormat("VersionFile not found.\n{0}", versionFilePath);
+
+		        return false;
+	        }
+
+	        var commitHash = string.Empty;
+
+	        using (var streamReader = new StreamReader(versionFilePath, Encoding.UTF8, false))
+	        {
+		        commitHash = await streamReader.ReadLineAsync();
+	        }
+
+	        if (string.IsNullOrEmpty(commitHash))
+	        {
+		        Debug.LogError("CommitHash not found.");
+
+		        return false;
+	        }
+
+			// アップロード.
+
+	        var sw = System.Diagnostics.Stopwatch.StartNew();
+
+	        var result = await uploader.Execute(exportPath, commitHash);
+
+	        sw.Stop();
+
+	        using (new DisableStackTraceScope())
+	        {
+		        if (result)
+		        {
+			        Debug.LogFormat("Upload Complete. ({0:F2}sec)\n\nVersion: {1}", sw.Elapsed.TotalSeconds, commitHash);
+		        }
+		        else
+		        {
+			        Debug.LogError("Upload Failed.");
+		        }
+	        }
+
+	        return result;
+        }
+    }
+}
