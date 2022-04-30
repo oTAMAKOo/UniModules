146ba5578c3bdc7714f24b7cc66a5cb9cece1d4c
oTAMAKOo <oTAMAKOo@hotmail.co.jp>
2022-05-01 04:54:34 +0900

    正しくキャッシュできていなかった問題修正

--- Scripts/Modules/Cache/SpriteAtlasCache.cs
+++ Scripts/Modules/Cache/SpriteAtlasCache.cs
@@ -1,10 +1,12 @@
-﻿
+
 using UnityEngine;
 using UnityEngine.U2D;
+using System;
+using Extensions;
 
 namespace Modules.Cache
 {
-    public sealed class SpriteAtlasCache
+    public sealed class SpriteAtlasCache : IDisposable
     {
         //----- params -----
 
@@ -13,6 +15,8 @@ namespace Modules.Cache
         private SpriteAtlas spriteAtlas = null;
 
         private Cache<Sprite> spriteCache = null;
+
+		private bool disposed = false;
 
         //----- property -----
 
@@ -27,6 +31,22 @@ namespace Modules.Cache
             spriteCache = new Cache<Sprite>(referenceName);
         }
 
+		~SpriteAtlasCache()
+		{
+			Dispose();
+		}
+
+		public void Dispose()
+		{
+			if (disposed) { return; }
+
+			Clear();
+			
+			disposed = true;
+
+			GC.SuppressFinalize(this);
+		}
+
         public Sprite GetSprite(string spriteName)
         {
             if (spriteAtlas == null) { return null; }
@@ -35,12 +55,28 @@ namespace Modules.Cache
 
             var sprite = spriteCache.Get(spriteName);
 
-            if (sprite != null) { return sprite; }
+            if (sprite == null)
+			{
+	            sprite = spriteAtlas.GetSprite(spriteName);
 
-            sprite = spriteAtlas.GetSprite(spriteName);
+				spriteCache.Add(spriteName, sprite);
+			}
 
             return sprite;
         }
+
+		public void Clear()
+		{
+			if (spriteCache != null)
+			{
+				var cahcedSprites = spriteCache.Values;
+
+				foreach (var sprite in cahcedSprites)
+				{
+					UnityUtility.SafeDelete(sprite);
+				}
+			}
+		}
 
         public bool HasCache(string spriteName)
         {
