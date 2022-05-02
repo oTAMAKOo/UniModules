77ffb22fd341876734ad5506a3331cf56dc94b0c
oTAMAKOo <oTAMAKOo@hotmail.co.jp>
2022-05-02 17:40:53 +0900

    ローカライズ周り調整

--- Scripts/Modules/Localize/Language/Editor/LanguageSelector.cs
+++ Scripts/Modules/Localize/Language/Editor/LanguageSelector.cs
@@ -1,44 +1,76 @@
-
 using UnityEngine;
 using UnityEditor;
 using System;
+using System.IO;
+using System.Reflection;
 using Extensions.Devkit;
+using Modules.Devkit.Prefs;
 using Modules.TextData.Editor;
 
 namespace Modules.Localize.Editor
 {
-    public sealed class LanguageSelector : SingletonEditorWindow<LanguageSelector>
-    {
-        //----- params -----
+	public sealed class LanguageSelector : SingletonEditorWindow<LanguageSelector>
+	{
+		//----- params -----
 
-		public const string WindowTitle = "LanguageSelector";
+		public const string WindowTitle = "Language";
 
-        //----- field -----
+		private sealed class Prefs
+		{
+			public static string assembly
+			{
+				get { return ProjectPrefs.GetString(typeof(Prefs).FullName + "-assembly", null); }
+				set { ProjectPrefs.SetString(typeof(Prefs).FullName + "-assembly", value); }
+			}
+
+			public static string enumType
+			{
+				get { return ProjectPrefs.GetString(typeof(Prefs).FullName + "-enumType", null); }
+				set { ProjectPrefs.SetString(typeof(Prefs).FullName + "-enumType", value); }
+			}
+		}
+
+		//----- field -----
 
 		private Type enumType = null;
 
-        //----- property -----
+		//----- property -----
 
-        //----- method -----
+		//----- method -----
 
 		public static void Open(Type enumType)
 		{
-			Instance.Initialize(enumType);
+			if (enumType != null)
+			{
+				Prefs.assembly = enumType.Assembly.FullName;
+				Prefs.enumType = enumType.FullName;
+			}
+
+			Instance.Initialize();
 		}
 
-		private void Initialize(Type enumType)
+		private void Initialize()
 		{
-			this.enumType = enumType;
-
 			titleContent = new GUIContent(WindowTitle);
 
 			minSize = new Vector2(100, 35f);
+
+			if (string.IsNullOrEmpty(Prefs.assembly) || string.IsNullOrEmpty(Prefs.enumType))
+			{
+				throw new InvalidDataException();
+			}
+            
+			var assembly = Assembly.Load(Prefs.assembly);
+
+			enumType = assembly.GetType(Prefs.enumType);
 
 			Show(true);
 		}
 
 		void OnGUI()
 		{
+			Initialize();
+
 			GUILayout.Space(8f);
 
 			EditorGUI.BeginChangeCheck();
@@ -56,5 +88,5 @@ namespace Modules.Localize.Editor
 
 			GUILayout.Space(8f);
 		}
-    }
+	}
 }
