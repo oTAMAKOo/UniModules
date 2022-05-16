92d0f98da9779d105c526a14495156914888c25e
oTAMAKOo <oTAMAKOo@hotmail.co.jp>
2022-05-16 12:57:25 +0900

    ブランチ名取得追加

--- Scripts/Extensions/Utility/Editor/GitUtility.cs
+++ Scripts/Extensions/Utility/Editor/GitUtility.cs
@@ -5,6 +5,31 @@ namespace Extensions
 {
     public static class GitUtility
     {
+        /// <summary> 現在のブランチ名を取得 </summary>
+        public static async Task<string> GetBranchName(string workingDirectory, bool trimAsterisk = true)
+        {
+            if (string.IsNullOrEmpty(workingDirectory)){ return null; }
+
+            var processExecute = new ProcessExecute("git", "branch --contains")
+            {
+                WorkingDirectory = workingDirectory,
+            };
+
+            var result = await processExecute.StartAsync();
+
+            if (string.IsNullOrEmpty(result.Output)){ return null; }
+
+            // 改行コードを削除.
+            var branchName = result.Output.Replace("\r", "").Replace("\n", "");
+
+            if (trimAsterisk)
+            {
+                branchName = branchName.TrimStart('*', ' ');
+            }
+
+            return branchName;
+        }
+
 		/// <summary> 現在のコミットハッシュを取得 </summary>
 		public static async Task<string> GetCommitHash(string workingDirectory)
 		{
