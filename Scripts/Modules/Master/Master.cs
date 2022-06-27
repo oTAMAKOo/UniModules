37b53b2be29bd3f08558d2d8a75cf93929c8e92b
oTAMAKOo <oTAMAKOo@hotmail.co.jp>
2022-06-27 13:56:04 +0900

    newできるようにする

--- Scripts/Modules/Master/Master.cs
+++ Scripts/Modules/Master/Master.cs
@@ -28,7 +28,7 @@ namespace Modules.Master
     public abstract class Master<TKey, TMaster, TMasterContainer, TMasterRecord> : IMaster
         where TKey : IComparable
         where TMaster : Master<TKey, TMaster, TMasterContainer, TMasterRecord>, new()
-        where TMasterContainer : MasterContainer<TMasterRecord>
+        where TMasterContainer : MasterContainer<TMasterRecord>, new()
     {
         //----- params -----
 
