8248e6225d81c005faa584102c806e3ac34ea7a3
ma-takeuchi <ma-takeuchi@nextninja.co.jp>
2022-07-30 08:38:44 +0900

    空のルビで行頭がズレる問題修正

--- Scripts/Modules/Scenario/RubyTextMeshProUGUIExtension.cs
+++ Scripts/Modules/Scenario/RubyTextMeshProUGUIExtension.cs
@@ -6,57 +6,86 @@ using Extensions;
 
 namespace TMPro
 {
-	public static class RubyTextMeshProUGUIExtension
-	{
-		private static FieldInfo rubyScaleFieldInfo = null;
+    public static class RubyTextMeshProUGUIExtension
+    {
+        private static FieldInfo rubyScaleFieldInfo = null;
+        private static FieldInfo maxFontSizeFieldInfo = null;
 
-		/// <summary> ルビタグを含む行の先頭に空のルビタグを追加  </summary>
-		public static string InsertEmptyRubyTag(this RubyTextMeshProUGUI self, string text)
-		{
-			if (string.IsNullOrEmpty(text)){ return text; }
+        /// <summary> ルビタグを含む行の先頭に空のルビタグを追加  </summary>
+        public static string InsertEmptyRubyTag(this RubyTextMeshProUGUI self, string text)
+        {
+            if (string.IsNullOrEmpty(text)){ return text; }
 
-			var s1 = self.GetPreferredValues("\u00A0a").x;
-			var s2 = self.GetPreferredValues("a").x;
+            // 空白のルビタグ作成.
 
-			var spaceW = (s1  - s2) * GetRubyScale(self);
+            var fontSizeScale = 1f;
+
+            if (self.enableAutoSizing)
+            {
+                fontSizeScale = self.fontSize / GetMaxFontSize(self);
+            }
+
+            var rightToLeft = self.isRightToLeftText ? 1 : -1;
+
+            var s1 = self.GetPreferredValues("\u00A0a").x;
+            var s2 = self.GetPreferredValues("a").x;
+
+            var rubyScale = GetRubyScale(self);
+
+            var spaceW = (s1 - s2) * rubyScale * fontSizeScale * rightToLeft;
+
+            var emptyRubyText = $"<space=-{spaceW}><ruby= ></ruby>";
+
+            // 文字列解析.
+
+            var builder = new StringBuilder();
+
+            var lines = text.FixLineEnd().Split('\n');
             
-			var emptyRubyText = $"<space=-{spaceW}><ruby= ></ruby>";
+            var rubyTagRegex = new Regex("<ruby=[^>]*?>", RegexOptions.Compiled);
 
-			var builder = new StringBuilder();
+            foreach (var line in lines)
+            {
+                if (0 < builder.Length)
+                {
+                    builder.AppendLine();
+                }
 
-			var lines = text.FixLineEnd().Split('\n');
-            
-			var rubyTagRegex = new Regex("<ruby=[^>]*?>", RegexOptions.Compiled);
-
-			foreach (var line in lines)
-			{
-				if (0 < builder.Length)
-				{
-					builder.AppendLine();
-				}
-
-				if (rubyTagRegex.IsMatch(line))
-				{
-					builder.Append(emptyRubyText);
-				}
+                if (rubyTagRegex.IsMatch(line))
+                {
+                    builder.Append(emptyRubyText);
+                }
 				
-				builder.Append(line);
-			}
+                builder.Append(line);
+            }
 
-			return builder.ToString();
-		}
+            return builder.ToString();
+        }
 
-		private static float GetRubyScale(RubyTextMeshProUGUI textMeshPro)
-		{
-			if (rubyScaleFieldInfo == null)
-			{
-				var type = typeof(RubyTextMeshProUGUI);
-				var flag = BindingFlags.NonPublic | BindingFlags.Instance;
+        private static float GetMaxFontSize(RubyTextMeshProUGUI textMeshPro)
+        {
+            if (rubyScaleFieldInfo == null)
+            {
+                var type = typeof(RubyTextMeshProUGUI);
+                var flag = BindingFlags.NonPublic | BindingFlags.Instance;
 
-				rubyScaleFieldInfo = Reflection.GetFieldInfo(type, "rubyScale", flag);
-			}
+                maxFontSizeFieldInfo = Reflection.GetFieldInfo(type, "m_maxFontSize", flag);
+            }
 
-			return (float)rubyScaleFieldInfo.GetValue(textMeshPro);
-		}
-	}
+            return (float)maxFontSizeFieldInfo.GetValue(textMeshPro);
+        }
+
+        private static float GetRubyScale(RubyTextMeshProUGUI textMeshPro)
+        {
+            if (rubyScaleFieldInfo == null)
+            {
+                var type = typeof(RubyTextMeshProUGUI);
+                var flag = BindingFlags.NonPublic | BindingFlags.Instance;
+
+                rubyScaleFieldInfo = Reflection.GetFieldInfo(type, "rubyScale", flag);
+            }
+
+            return (float)rubyScaleFieldInfo.GetValue(textMeshPro);
+        }
+    }
 }
