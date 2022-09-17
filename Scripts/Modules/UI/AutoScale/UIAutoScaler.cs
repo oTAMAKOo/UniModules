b3543f19b4d995dc594e384cde6441f5bb67c4b6
oTAMAKOo <oTAMAKOo@hotmail.co.jp>
2022-09-18 02:16:07 +0900

    Revert "使わないので削除"
    This reverts commit a9a31d732303b6bf44fa20d1df91630428e61491.

---
+++ Scripts/Modules/UI/AutoScale/UIAutoScaler.cs
@@ -0,0 +1,56 @@
+﻿﻿﻿
+using UnityEngine;
+using UnityEngine.EventSystems;
+using Extensions;
+
+namespace Modules.UI
+{
+    [ExecuteAlways]
+	public sealed class UIAutoScaler : UIBehaviour
+	{
+		//----- params -----
+
+		//----- field -----
+
+        [SerializeField]
+        private RectTransform target = null;
+        [SerializeField]
+        private Vector2 baseSize = Vector2.zero;
+
+        private Vector2? targetSize = null;
+
+        //----- property -----
+
+        //----- method -----
+
+        void Update()
+        {
+            if(UnityUtility.IsNull(target)) { return; }
+
+            UpdateScale();
+        }
+
+        public void UpdateScale()
+        {
+            var size = target.GetSize();
+
+            if (targetSize.HasValue && targetSize.Value.x == size.x && targetSize.Value.y == size.y)
+            {
+                return;
+            }
+
+            targetSize = size;
+
+            var rt = transform as RectTransform;
+
+            rt.SetSize(baseSize);
+
+            rt.localScale = new Vector3()
+            {
+                x = targetSize.Value.x / baseSize.x,
+                y = targetSize.Value.y / baseSize.y,
+                z = 1f,
+            };
+        }
+	}
+}
