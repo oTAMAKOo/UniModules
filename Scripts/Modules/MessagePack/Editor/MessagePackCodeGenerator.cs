59ca9785e117b9da463a12efda84bc72c01ced51
oTAMAKOo <oTAMAKOo@hotmail.co.jp>
2022-10-25 06:22:34 +0900

    コードジェネレータ最新対応

--- Scripts/Modules/MessagePack/Editor/MessagePackCodeGenerator.cs
+++ Scripts/Modules/MessagePack/Editor/MessagePackCodeGenerator.cs
@@ -111,17 +111,8 @@ namespace Modules.MessagePack
 
             if (string.IsNullOrEmpty(command))
             {
-	            command = GetMpcCommand();
-
-				#if UNITY_EDITOR_OSX
-
-				if (command.EndsWith("dotnet"))
-				{
-					argument = "mpc " + argument;
-				}
-
-				#endif
-            }
+	            command = "mpc";
+			}
 
             var processExecute = new ProcessExecute(command, argument)
             {
@@ -131,57 +122,33 @@ namespace Modules.MessagePack
             return processExecute;
         }
 
-        private static string GetMpcCommand()
-        {
-	        var result = "mpc";
 
-			#if UNITY_EDITOR_WIN
-
-	        // 環境変数.
-	        var variable = Environment.GetEnvironmentVariable("Path", EnvironmentVariableTarget.Process);
-
-	        if (variable != null)
-	        {
-		        foreach (var item in variable.Split(';'))
-		        {
-			        var path = PathUtility.Combine(item, "mpc.exe");
-
-			        if (!File.Exists(path)){ continue; }
-
-			        result = path;
-
-			        break;
-		        }
-	        }
-			
-			#endif
-
-			#if UNITY_EDITOR_OSX
-
-			var mpcPathCandidate = new string[]
-			{
-				"/usr/local/bin/",
-			};
-
-			foreach (var item in mpcPathCandidate)
-			{
-				var path = PathUtility.Combine(item, "dotnet");
-
-				if (!File.Exists(path)){ continue; }
-
-				result = path;
-
-				break;
-			}
-
-			#endif
-
-	        return result;
-        }
-
-        private static void ImportGeneratedCsFile(string csFilePath, string csFileHash)
+		private static void ImportGeneratedCsFile(string csFilePath, string csFileHash)
         {
             var assetPath = UnityPathUtility.ConvertFullPathToAssetPath(csFilePath);
+            
+            // global::UnityEngineが定義されない問題対応.
+            
+            var builder = new StringBuilder();
+
+            var encode = new UTF8Encoding(true);
+
+            using (var sr = new StreamReader(csFilePath, encode))
+            {
+                var code = sr.ReadToEnd();
+
+                builder.AppendLine("// Fix : Missing global::UnityEngine error.");
+                builder.AppendLine("using UnityEngine;");
+                builder.AppendLine();
+                builder.Append(code);
+            }
+
+            using (var sw = new StreamWriter(csFilePath, false, encode))
+            {
+                sw.Write(builder.ToString());
+            }
+
+            // 差分があったらインポート.
 
             var hash = GetCsFileHash(csFilePath);
 
