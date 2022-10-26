0166ad5896d01a56bca724db047d2ba02624685f
oTAMAKOo <oTAMAKOo@hotmail.co.jp>
2022-10-26 16:35:59 +0900

    Revert "消したらダメな処理だったので戻す"
    This reverts commit 9ea58e63e748f26ecc2af976dca38cdb13a8b0b1.

--- Scripts/Modules/ExternalResources/AssetBundle/AssetBundleManager.cs
+++ Scripts/Modules/ExternalResources/AssetBundle/AssetBundleManager.cs
@@ -709,8 +709,6 @@ namespace Modules.AssetBundles
                 UnloadDependencies(target, unloadAllLoadedObjects, force);
                 UnloadAssetBundleInternal(target, unloadAllLoadedObjects, force);
             }
-
-			dependencies.Remove(assetBundleName);
         }
 
         private void UnloadAssetBundleInternal(string assetBundleName, bool unloadAllLoadedObjects, bool force = false)
