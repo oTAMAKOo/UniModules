2d4570aa45700cad6fb9fce26eec994ff694b5d1
oTAMAKOo <oTAMAKOo@hotmail.co.jp>
2023-01-15 06:25:00 +0900

    デフォルト値変更

--- Scripts/Modules/ExternalAsset/ExternalAsset.assetbundle.cs
+++ Scripts/Modules/ExternalAsset/ExternalAsset.assetbundle.cs
@@ -18,7 +18,7 @@ namespace Modules.ExternalAssets
         //----- params -----
 
 		/// <summary> 最大同時ダウンロード数. </summary>
-		private const uint AssetBundleDefaultInstallerCount = 4;
+		private const uint AssetBundleDefaultInstallerCount = 8;
 
         //----- field -----
 
