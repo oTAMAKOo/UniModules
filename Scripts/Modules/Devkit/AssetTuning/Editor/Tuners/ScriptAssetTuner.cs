2ad862818b8e934c3abd54d917991650adb40e66
oTAMAKOo <oTAMAKOo@hotmail.co.jp>
2023-03-24 01:24:30 +0900

    BOMが重複する事があった問題修正

--- Scripts/Modules/Devkit/AssetTuning/Editor/Tuners/ScriptAssetTuner.cs
+++ Scripts/Modules/Devkit/AssetTuning/Editor/Tuners/ScriptAssetTuner.cs
@@ -1,28 +1,27 @@
-﻿﻿
 using Extensions;
 using System.IO;
 using System.Text;
 
 namespace Modules.Devkit.AssetTuning
 {
-    public sealed class ScriptAssetTuner : AssetTuner
-    {
-        //----- params -----
+	public sealed class ScriptAssetTuner : AssetTuner
+	{
+		//----- params -----
 
-        //----- field -----
+		//----- field -----
 
-        //----- property -----
+		//----- property -----
 
-        public override int Priority { get { return 100; } }
+		public override int Priority { get { return 100; } }
 
-        //----- method -----
+		//----- method -----
 
-        public override bool Validate(string path)
-        {
-            var extension = Path.GetExtension(path);
+		public override bool Validate(string path)
+		{
+			var extension = Path.GetExtension(path);
             
-            return extension == ".cs";
-        }
+			return extension == ".cs";
+		}
 
 		public override void OnPreprocessAsset(string assetPath)
 		{
@@ -30,26 +29,31 @@ namespace Modules.Devkit.AssetTuning
 
 			if(!File.Exists(path)){ return; }
 
-            byte[] bytes = null;
+			byte[] bytes = null;
 
-            using (var fs = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
-            {
-                bytes = new byte[fs.Length];
+			using (var fs = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+			{
+				bytes = new byte[fs.Length];
 
-                fs.Read(bytes, 0, bytes.Length);
-            }
+				fs.Read(bytes, 0, bytes.Length);
+			}
             
-            var encode = Encode.GetEncode(bytes);
+			var encode = Encode.GetEncode(bytes);
 
-            if(encode != null)
-            {
+			if(encode != null)
+			{
 				var str = encode.GetString(bytes);
 
 				// UTF8 + BOM付きか確認.
-				var isUTF8Encoding = encode.CodePage == 65001 && bytes[0] == 0xEF && bytes[1] == 0xBB && bytes[2] == 0xBF;
+				var isUTF8Encoding = encode.CodePage == 65001;
+                
+				if (isUTF8Encoding)
+				{
+					str = str.TrimStart('\ufeff');
+				}
 
-                // 改行コードの置き換え.
-                var requireLineEndReplace = str.Contains("\r\n");
+				// 改行コードの置き換え.
+				var requireLineEndReplace = str.Contains("\r\n");
 
 				if (requireLineEndReplace)
 				{
@@ -57,12 +61,12 @@ namespace Modules.Devkit.AssetTuning
 					str = str.Replace("\r\n", "\n");
 				}
 
-                // 保存.
-				if (!isUTF8Encoding || requireLineEndReplace)
+				// 保存.
+				if (!isUTF8Encoding|| requireLineEndReplace)
 				{
 					File.WriteAllText(path, str, new UTF8Encoding(true));
 				}
-            }
-        }
-    }
+			}
+		}
+	}
 }
