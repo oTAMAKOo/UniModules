8b24f2f94360c0d1302f6deb0a48dab3ea9f6ca4
ma-takeuchi <ma-takeuchi@nextninja.co.jp>
2023-04-28 15:24:12 +0900

    フォントアセットに毎回差分が出る問題対応

---
+++ Scripts/Modules/Devkit/TextMeshPro/Editor/DynamicFontAssetModificationProcessor.cs
@@ -0,0 +1,36 @@
+﻿
+using UnityEditor;
+using System.IO;
+using TMPro;
+using Extensions.Devkit;
+
+namespace Modules.Devkit.TextMeshPro
+{
+	public class DynamicFontAssetModificationProcessor : UnityEditor.AssetModificationProcessor
+	{
+		private const string FontAssetExtension = ".asset";
+
+		static string[] OnWillSaveAssets(string[] paths)
+		{
+			using (new AssetEditingScope())
+			{
+				foreach (var path in paths)
+				{
+					var extension = Path.GetExtension(path);
+
+					if (extension != FontAssetExtension){ continue; }
+
+					var fontAsset = AssetDatabase.LoadAssetAtPath(path, typeof(TMP_FontAsset)) as TMP_FontAsset;
+
+					if (fontAsset == null){ continue; }
+				
+					if (fontAsset.atlasPopulationMode != AtlasPopulationMode.Dynamic){ continue; }
+					
+					fontAsset.ClearFontAssetData(true);
+				}
+			}
+
+			return paths;
+		}
+	}
+} 
