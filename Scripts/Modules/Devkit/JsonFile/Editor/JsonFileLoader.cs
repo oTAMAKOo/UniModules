9392a142ddb5a2495585c60234557d97011405b7
oTAMAKOo <oTAMAKOo@hotmail.co.jp>
2023-06-03 05:07:30 +0900

    相対パスのJsonファイル読み込みを補助する機能追加

---
+++ Scripts/Modules/Devkit/JsonFile/Editor/JsonFileLoader.cs
@@ -0,0 +1,40 @@
+﻿
+using UnityEngine;
+using System;
+using System.IO;
+using Newtonsoft.Json;
+using Extensions;
+
+namespace Modules.Devkit.JsonFile
+{
+    [Serializable]
+    public sealed class JsonFileLoader
+    {
+        //----- params -----
+
+        //----- field -----
+        
+        [SerializeField]
+        private string jsonFileRelativePath = null;
+
+        //----- property -----
+
+        //----- method -----
+
+        public T Load<T>() where T : class
+        {
+            var filePath = UnityPathUtility.RelativePathToFullPath(jsonFileRelativePath);
+
+            if (!File.Exists(filePath))
+            {
+                throw new FileNotFoundException(filePath);
+            }
+
+            var json = File.ReadAllText(filePath);
+
+            var config = JsonConvert.DeserializeObject<T>(json);
+
+            return config;
+        }
+    }
+}
---
+++ Scripts/Modules/Devkit/JsonFile/Editor/JsonFileLoaderPropertyDrawer.cs
@@ -0,0 +1,60 @@
+﻿
+using UnityEngine;
+using UnityEditor;
+using System;
+using Extensions;
+using Extensions.Devkit;
+
+namespace Modules.Devkit.JsonFile
+{
+    [CustomPropertyDrawer(typeof(JsonFileLoader))]
+    public sealed class JsonFileLoaderPropertyDrawer : PropertyDrawer
+    {
+        //----- params -----
+
+        //----- field -----
+        
+        //----- property -----
+
+        //----- method -----
+
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            const float EditButtonWidth = 60f;
+            const float Space = 2f;
+            
+            var serializedObject = property.serializedObject;
+
+            var jsonFileRelativePathProperty = property.FindPropertyRelative("jsonFileRelativePath");
+            
+            position = EditorGUI.PrefixLabel(position,  GUIUtility.GetControlID(FocusType.Passive), new GUIContent("Json File Relative Path"));
+
+            position.width -= EditButtonWidth + Space;
+
+            jsonFileRelativePathProperty.stringValue = EditorGUI.TextField(position, jsonFileRelativePathProperty.stringValue);
+
+            position.x += position.width + Space;
+            position.width = EditButtonWidth;
+
+            if (GUI.Button(position, "edit", EditorStyles.miniButton))
+            {
+                EditorApplication.delayCall += () =>
+                {
+                    var projectFolderPath = UnityPathUtility.GetProjectFolderPath();
+
+                    var jsonFilePath = EditorUtility.OpenFilePanel("Select Json", projectFolderPath, "json");
+
+                    if (!string.IsNullOrEmpty(jsonFilePath))
+                    {
+                        var assetFolderUri = new Uri(Application.dataPath);
+                        var targetUri = new Uri(jsonFilePath);
+
+                        jsonFileRelativePathProperty.stringValue = assetFolderUri.MakeRelativeUri(targetUri).ToString();
+
+                        serializedObject.ApplyModifiedProperties();
+                    }
+                };
+            }
+        }
+    }
+}
