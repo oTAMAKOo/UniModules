e6ffc6055c3853c932bde0b504062d23c7228ea4
oTAMAKOo <oTAMAKOo@hotmail.co.jp>
2023-06-25 12:48:34 +0900

    Animatorの拡張追加

---
+++ Scripts/Extensions/Methods/AnimatorExtensions.cs
@@ -0,0 +1,14 @@
+﻿
+using UnityEngine;
+
+namespace Extensions
+{
+    public static class AnimatorExtensions
+    {
+        /// <summary> Animatorが利用可能か </summary>
+        public static bool IsAvailable(this Animator self)
+        {
+            return self.gameObject.activeInHierarchy && self.runtimeAnimatorController != null;
+        }
+    }
+}
