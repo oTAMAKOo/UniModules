b1700669d385ad83da1e8e19ea52ad25a26573e8
oTAMAKOo <oTAMAKOo@hotmail.co.jp>
2023-07-06 19:53:11 +0900

    URPのカメラ制御追加

---
+++ Scripts/Modules/Rendering/Rendering/Universal/CameraStackManager.cs
@@ -0,0 +1,106 @@
+﻿
+#if ENABLE_UNIVERSALRENDERPIPELINE
+
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Extensions;
+
+namespace Modules.Rendering.Universal
+{
+    public sealed class CameraStackManager : Singleton<CameraStackManager>
+    {
+        //----- params -----
+
+        //----- field -----
+
+        private Camera currentCamera = null;
+        
+        private List<CameraStackTarget> cameraStackTargets = null;
+
+        //----- property -----
+
+        //----- method -----
+
+        protected override void OnCreate()
+        {
+            cameraStackTargets = new List<CameraStackTarget>();
+        }
+
+        public void SwitchMainCamera(Camera camera)
+        {
+            if (camera == null){ return; }
+
+            var prevCamera = currentCamera;
+
+            currentCamera = camera;
+
+            //------ Remove Prev Camera ------
+
+            var prevCameraData = UnityUtility.GetComponent<UniversalAdditionalCameraData>(prevCamera);
+
+            if (prevCameraData != null)
+            {
+                prevCameraData.cameraStack.Clear();
+            }
+            
+            //------ Setup New Camera ------
+
+            UpdateCurrentCameraStack();
+        }
+
+        public void UpdateCurrentCameraStack()
+        {
+            if (currentCamera == null){ return; }
+
+            var currentCameraData = UnityUtility.GetComponent<UniversalAdditionalCameraData>(currentCamera);
+
+            if (currentCameraData != null)
+            {
+                currentCameraData.cameraStack.Clear();
+
+                cameraStackTargets = cameraStackTargets
+                    .Where(x => !UnityUtility.IsNull(x))
+                    .OrderBy(x => x.Priority)
+                    .ToList();
+
+                foreach (var element in cameraStackTargets)
+                {
+                    currentCameraData.cameraStack.Add(element.Camera);
+                }
+            }
+        }
+
+        public void AddStackCamera(Camera camera)
+        {
+            if (camera == null){ return; }
+
+            var cameraStackTarget = UnityUtility.GetComponent<CameraStackTarget>(camera);
+
+            if (cameraStackTarget == null){ return; }
+
+            if (!cameraStackTargets.Contains(cameraStackTarget))
+            {
+                cameraStackTargets.Add(cameraStackTarget);
+            }
+        }
+
+        public void RemoveStackCamera(Camera camera)
+        {
+            if (camera == null){ return; }
+
+            var cameraStackTarget = UnityUtility.GetComponent<CameraStackTarget>(camera);
+
+            if (cameraStackTarget == null){ return; }
+
+            if (cameraStackTargets.Contains(cameraStackTarget))
+            {
+                cameraStackTargets.Remove(cameraStackTarget);
+            }
+        }
+    }
+}
+
+#endif
---
+++ Scripts/Modules/Rendering/Rendering/Universal/CameraStackTarget.cs
@@ -0,0 +1,59 @@
+﻿
+#if ENABLE_UNIVERSALRENDERPIPELINE
+using Extensions;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+namespace Modules.Rendering.Universal
+{
+    [RequireComponent(typeof(UniversalAdditionalCameraData))]
+    public sealed class CameraStackTarget : MonoBehaviour
+    {
+        //----- params -----
+
+        //----- field -----
+
+        [SerializeField]
+        private uint priority = 0;
+        [SerializeField]
+        private bool autoStack = false;
+
+        private Camera targetCamera = null;
+        private UniversalAdditionalCameraData cameraData = null;
+
+        //----- property -----
+
+        public uint Priority { get { return priority; } }
+
+        public Camera Camera
+        {
+            get
+            {
+                return targetCamera ?? (targetCamera = UnityUtility.GetComponent<Camera>(gameObject));
+            }
+        }
+
+        public UniversalAdditionalCameraData CameraData
+        {
+            get
+            {
+                return cameraData ?? (cameraData = UnityUtility.GetComponent<UniversalAdditionalCameraData>(gameObject));
+            }
+        }
+
+        //----- method -----
+
+        void OnEnable()
+        {
+            var cameraStackManager = CameraStackManager.Instance;
+
+            if (autoStack)
+            {
+                cameraStackManager.AddStackCamera(Camera);
+                cameraStackManager.UpdateCurrentCameraStack();
+            }
+        }
+    }
+}
+
+#endif
