178cfab77edaee7e1409897231658eaa499200de
oTAMAKOo <oTAMAKOo@hotmail.co.jp>
2023-08-04 22:50:56 +0900

    PlayFabの拡張追加

---
+++ Scripts/Modules/PlayFab/PlayFabResultExtensions.cs
@@ -0,0 +1,18 @@
+﻿
+#if ENABLE_PLAYFAB_CSHARP
+
+using PlayFab;
+using PlayFab.Internal;
+
+namespace Modules.PlayFabCSharp
+{
+    public static class PlayFabResultExtensions
+    {
+        public static bool HasError<TResult>(this PlayFabResult<TResult> self) where TResult : PlayFabResultCommon
+        {
+            return self.Error != null || self.Result == null;
+        }
+    }
+}
+
+#endif
