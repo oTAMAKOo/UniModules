49f64430f7eb0cd377a6af02fd43580f03e5fe97
oTAMAKOo <oTAMAKOo@hotmail.co.jp>
2023-08-15 11:27:43 +0900

    サフィックス変更できるように修正

--- Scripts/Extensions/Utility/ByteDataUtility.cs
+++ Scripts/Extensions/Utility/ByteDataUtility.cs
@@ -1,8 +1,47 @@
 
+using System.Collections.Generic;
+
 namespace Extensions
 {
     public static class ByteDataUtility
     {
+        public enum SizeType
+        {
+            /// <summary> バイト </summary>
+            B,
+
+            /// <summary> キロバイト (1000 B) </summary>
+            KB,
+
+            /// <summary> メガバイト (1000 kB) </summary>
+            MB,
+
+            /// <summary> ギガバイト (1000 MB) </summary>
+            GB,
+
+            /// <summary> テラバイト (1000 GB) </summary>
+            TB,
+
+            /// <summary> ペタバイト (1000 TB) </summary>
+            PB,
+
+            /// <summary> エクサバイト (1000 PB) </summary>
+            EB,
+        }
+
+        public static readonly Dictionary<SizeType, string> DefaultSuffix = new Dictionary<SizeType, string>()
+        {
+            { SizeType.B, "B" },
+            { SizeType.KB, "KB" },
+            { SizeType.MB, "MB" },
+            { SizeType.GB, "GB" },
+            { SizeType.TB, "TB" },
+            { SizeType.PB, "PB" },
+            { SizeType.EB, "EB" },
+        };
+
+        public static Dictionary<SizeType, string> Suffix = new Dictionary<SizeType, string>(DefaultSuffix);
+
         /// <summary> バイト数を100MBのような表示用文字列に変換 </summary>
         public static string GetBytesReadable(long byteSize, string format = "0.###")
         {
@@ -14,37 +53,37 @@ namespace Extensions
 
             if (byteSize >= 0x1000000000000000)
             {
-                suffix = "EB";
+                suffix = Suffix[SizeType.EB];
                 readable = (byteSize >> 50);
             }
             else if (byteSize >= 0x4000000000000)
             {
-                suffix = "PB";
+                suffix = Suffix[SizeType.PB];
                 readable = (byteSize >> 40);
             }
             else if (byteSize >= 0x10000000000)
             {
-                suffix = "TB";
+                suffix = Suffix[SizeType.TB];
                 readable = (byteSize >> 30);
             }
             else if (byteSize >= 0x40000000)
             {
-                suffix = "GB";
+                suffix = Suffix[SizeType.GB];
                 readable = (byteSize >> 20);
             }
             else if (byteSize >= 0x100000)
             {
-                suffix = "MB";
+                suffix = Suffix[SizeType.MB];
                 readable = (byteSize >> 10);
             }
             else if (byteSize >= 0x400)
             {
-                suffix = "KB";
+                suffix = Suffix[SizeType.KB];
                 readable = byteSize;
             }
             else
             {
-                return byteSize.ToString("0 B");
+                return byteSize.ToString("0") + Suffix[SizeType.B];
             }
 
             readable /= 1024;
