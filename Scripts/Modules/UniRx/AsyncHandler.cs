a9211acad651b7b8c7decdd8e4e8ae43ebd2dab8
oTAMAKOo <oTAMAKOo@hotmail.co.jp>
2023-09-04 04:36:44 +0900

    非同期待ちの為のクラス追加

---
+++ Scripts/Modules/UniRx/AsyncHandler.cs
@@ -0,0 +1,35 @@
+﻿
+using Cysharp.Threading.Tasks;
+
+namespace Modules.UniRxExtension
+{
+    public class AsyncHandler
+    {
+        //----- params -----
+
+        //----- field -----
+
+        private int count = 0;
+
+        //----- property -----
+
+        public bool IsComplete { get { return count <= 0; } }
+
+        //----- method -----
+
+        public void Begin()
+        {
+            count++;
+        }
+
+        public void End()
+        {
+            count--;
+        }
+
+        public async UniTask Wait()
+        {
+            await UniTask.WaitUntil(() => IsComplete);
+        }
+    }
+}
