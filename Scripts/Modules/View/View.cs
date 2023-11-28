8f1ebb0d3129cf3e206df4ef7f1435fc053ff469
oTAMAKOo <oTAMAKOo@hotmail.co.jp>
2023-11-29 04:14:09 +0900

    ViewBase継承しない場合でもViewModel取得できるように改修

---
+++ Scripts/Modules/View/View.cs
@@ -0,0 +1,85 @@
+﻿
+using UnityEngine;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using Unity.Linq;
+using Extensions;
+
+namespace Modules.View
+{
+    public interface IViewRoot
+    {
+        ViewModel GetViewModel();
+    }
+
+    public interface IView<TViewModel> where TViewModel : ViewModel, new(){  }
+
+    public static class ViewExtensions
+    {
+        private const int CacheRefreshFrameInterval = 20;
+
+        private static int cacheRefreshFrameCount = 0;
+
+        private static Dictionary<MonoBehaviour, ViewModel> viewModelCache = null;
+
+        public static TViewModel GetViewModel<TViewModel>(this IView<TViewModel> view) where TViewModel : ViewModel, new()
+        {
+            if (viewModelCache == null)
+            {
+                viewModelCache = new Dictionary<MonoBehaviour, ViewModel>();
+            }
+
+            CacheRefresh();
+
+            var monoBehaviour = view as MonoBehaviour;
+
+            if (monoBehaviour == null) { return null; }
+
+            var viewModel = viewModelCache.GetValueOrDefault(monoBehaviour);
+
+            if (viewModel != null)
+            {
+                if (viewModel is TViewModel) { return (TViewModel)viewModel; }
+            }
+
+            if (viewModel != null && viewModel.IsDisposed)
+            {
+                viewModel = null;
+            }
+
+            if (viewModel == null)
+            {
+                var viewRoot = monoBehaviour.gameObject.AncestorsAndSelf()
+                    .Select(x => UnityUtility.GetInterface<IViewRoot>(x))
+                    .FirstOrDefault(x => x != null);
+
+                if (viewRoot != null)
+                {
+                    viewModel = viewRoot.GetViewModel() as TViewModel;
+
+                    viewModelCache[monoBehaviour] = viewModel;
+                }
+                else
+                {
+                    throw new Exception("IViewRoot interface not found in ancestors hierarchy.");
+                }
+            }
+
+            return viewModel as TViewModel;
+        }
+
+        private static void CacheRefresh()
+        {
+            var frameCount = Time.frameCount;
+
+            if (frameCount < cacheRefreshFrameCount + CacheRefreshFrameInterval){ return; }
+
+            cacheRefreshFrameCount = frameCount;
+
+            viewModelCache = viewModelCache
+                .Where(x => !UnityUtility.IsNull(x.Key))
+                .ToDictionary(x => x.Key, x => x.Value);
+        }
+    }
+}
--- Scripts/Modules/View/ViewBase.cs
+++ Scripts/Modules/View/ViewBase.cs
@@ -1,62 +1,21 @@
 ﻿
 using UnityEngine;
-using Unity.Linq;
-using System.Linq;
-using Extensions;
 
 namespace Modules.View
 {
-    public interface IViewRoot
-    {
-        ViewModel GetViewModel();
-    }
-
-    public abstract class ViewBase<TViewModel> : MonoBehaviour where TViewModel : ViewModel, new()
+    public abstract class ViewBase<TViewModel> : MonoBehaviour, IView<TViewModel> where TViewModel : ViewModel, new()
     {
         //----- params -----
 
         //----- field -----
 
-        private TViewModel viewModel = null;
-
         //----- property -----
 
         protected TViewModel ViewModel
         {
-            get { return GetViewModel(); }
+            get { return this.GetViewModel(); }
         }
 
         //----- method -----
-
-        private TViewModel GetViewModel()
-        {
-            if (viewModel != null && viewModel.IsDisposed)
-            {
-                viewModel = null;
-            }
-
-            if (viewModel == null)
-            {
-                var viewRoot = gameObject.AncestorsAndSelf()
-                    .Select(x => UnityUtility.GetInterface<IViewRoot>(x))
-                    .FirstOrDefault(x => x != null);
-
-                if (viewRoot != null)
-                {
-                    viewModel = viewRoot.GetViewModel() as TViewModel;
-                }
-                else
-                {
-                    Debug.LogError("IViewRoot interface not found in ancestors hierarchy.");
-                }
-            }
-
-            return viewModel;
-        }
-
-        public void SetViewModel(TViewModel viewModel)
-        {
-            this.viewModel = viewModel;
-        }
     }
 }
