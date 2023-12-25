19238c0747cbc04a4f2c3266318fe695fb2e848c
oTAMAKOo <oTAMAKOo@hotmail.co.jp>
2023-12-26 01:05:17 +0900

    スクリーン座標を追従するUI機能追加

---
+++ Scripts/Modules/UI/ScreenPositionTracker.cs
@@ -0,0 +1,64 @@
+﻿
+using UnityEngine;
+using UnityEngine.EventSystems;
+using Unity.Linq;
+using System.Linq;
+using Extensions;
+
+namespace Modules.UI
+{
+    [ExecuteAlways]
+    public sealed class ScreenPositionTracker : UIBehaviour
+    {
+        //----- params -----
+
+        //----- field -----
+
+        [SerializeField]
+        private Transform target = null;
+        [SerializeField]
+        private Vector2 offset = Vector2.zero;
+
+        private Camera targetCamera = null;
+
+        //----- property -----
+
+        //----- method -----
+
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+
+            var rootCanvas = gameObject.Ancestors().OfComponent<Canvas>().FirstOrDefault(x => x.isRootCanvas);
+
+            if (rootCanvas != null)
+            {
+                targetCamera = rootCanvas.worldCamera;
+            }
+        }
+
+        void Update()
+        {
+            RefreshPosition();
+        }
+
+        private void RefreshPosition()
+        {
+            if (target == null){ return; }
+
+            if (targetCamera == null){ return; }
+
+            var screenPosition = targetCamera.WorldToScreenPoint(target.position);
+            
+            transform.position = targetCamera.ScreenToWorldPoint(screenPosition)  + offset.ToVector3();
+        }
+
+        public void SetTarget(Transform target, Vector2 offset)
+        {
+            this.target = target;
+            this.offset = offset;
+
+            RefreshPosition();
+        }
+    }
+}
