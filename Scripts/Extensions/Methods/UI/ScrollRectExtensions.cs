2aa25b2f48f672f4256f972799f3a8665a2a2df5
oTAMAKOo <oTAMAKOo@hotmail.co.jp>
2024-07-01 11:05:29 +0900

    拡張追加

---
+++ Scripts/Extensions/Methods/UI/ScrollRectExtensions.cs
@@ -0,0 +1,21 @@
+﻿
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Extensions
+{
+    public static class ScrollRectExtensions
+    {
+        public static void ScrollToItem(this ScrollRect scrollRect, GameObject target, Vector2? offset = null)
+        {
+            Canvas.ForceUpdateCanvases();
+
+            var rt = scrollRect.transform as RectTransform;
+
+            var p1 = (Vector2)rt.InverseTransformPoint(scrollRect.content.position);
+            var p2 = (Vector2)rt.InverseTransformPoint(target.transform.position);
+
+            scrollRect.content.anchoredPosition = p1 - p2 + (offset.HasValue ? offset.Value : Vector2.zero);
+        }
+    }
+}
