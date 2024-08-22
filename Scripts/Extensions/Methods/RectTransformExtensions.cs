3b7889a19ccb818fd11258dae14e80828d06d6ec
oTAMAKOo <oTAMAKOo@hotmail.co.jp>
2024-08-23 02:37:44 +0900

    関数追加

--- Scripts/Extensions/Methods/RectTransformExtensions.cs
+++ Scripts/Extensions/Methods/RectTransformExtensions.cs
@@ -186,6 +186,48 @@ namespace Extensions
             return new Vector2(width, hight);
         }
 
+        /// <summary> 重なっているかどうか </summary>
+        public static bool IsOverlap(this RectTransform rect1, RectTransform rect2) 
+        {
+            var rect1Corners = new Vector3[4];
+            var rect2Corners = new Vector3[4];
+
+            rect1.GetWorldCorners(rect1Corners);
+            rect2.GetWorldCorners(rect2Corners);
+
+            for (var i = 0; i < 4; i++) 
+            {
+                if (IsPointInsideRect(rect1Corners[i], rect2Corners)) 
+                {
+                    return true;
+                }
+
+                if (IsPointInsideRect(rect2Corners[i], rect1Corners))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        /// <summary> 指定座標が矩形の内部にあるか </summary>
+        private static bool IsPointInsideRect(Vector3 point, Vector3[] rectCorners) 
+        {
+            var inside = false;
+
+            for (int i = 0, j = 3; i < 4; j = i++) 
+            {
+                if (((rectCorners[i].y > point.y) != (rectCorners[j].y > point.y)) &&
+                    (point.x < (rectCorners[j].x - rectCorners[i].x) * (point.y - rectCorners[i].y) / (rectCorners[j].y - rectCorners[i].y) + rectCorners[i].x)) 
+                {
+                    inside = !inside;
+                }
+            }
+
+            return inside;
+        }
+
         /// <summary> 対象のRectTransformを内包しているか </summary>
         public static bool Contains(this RectTransform self, RectTransform target)
         {
