6f90e5128be08ec24b32b63bdc484924bb10b80a
oTAMAKOo <oTAMAKOo@hotmail.co.jp>
2024-11-08 02:19:55 +0900

    レイアウトがおかしくなる問題対応

--- Scripts/Extensions/Methods/UI/ScrollRectExtensions.cs
+++ Scripts/Extensions/Methods/UI/ScrollRectExtensions.cs
@@ -8,14 +8,26 @@ namespace Extensions
     {
         public static void ScrollToItem(this ScrollRect scrollRect, GameObject target, Vector2? offset = null)
         {
-            Canvas.ForceUpdateCanvases();
-
             var rt = scrollRect.transform as RectTransform;
+
+            rt.ForceRebuildLayoutGroup();
 
             var p1 = (Vector2)rt.InverseTransformPoint(scrollRect.content.position);
             var p2 = (Vector2)rt.InverseTransformPoint(target.transform.position);
 
-            scrollRect.content.anchoredPosition = p1 - p2 + (offset.HasValue ? offset.Value : Vector2.zero);
+            var anchoredPosition = scrollRect.content.anchoredPosition;
+
+            if (scrollRect.horizontal)
+            {
+                anchoredPosition.x = p1.x - p2.x + (offset.HasValue ? offset.Value.x : 0f);
+            }
+
+            if (scrollRect.vertical)
+            {
+                anchoredPosition.y = p1.y - p2.y + (offset.HasValue ? offset.Value.y : 0f);
+            }
+
+            scrollRect.content.anchoredPosition = anchoredPosition;
         }
     }
 }
