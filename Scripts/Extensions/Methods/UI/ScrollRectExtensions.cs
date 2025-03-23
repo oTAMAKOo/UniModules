3fe083035106ac387819386e73ed7b30b1732709
ma-takeuchi <otamakoo@gmail.com>
2025-03-23 09:01:08 +0900

    非同期スクロール追加

--- Scripts/Extensions/Methods/UI/ScrollRectExtensions.cs
+++ Scripts/Extensions/Methods/UI/ScrollRectExtensions.cs
@@ -1,6 +1,8 @@
-﻿
+
 using UnityEngine;
 using UnityEngine.UI;
+using Cysharp.Threading.Tasks;
+using DG.Tweening;
 
 namespace Extensions
 {
@@ -28,6 +30,30 @@ namespace Extensions
             }
 
             scrollRect.content.anchoredPosition = anchoredPosition;
+        }
+
+        public static async UniTask ScrollToItemAsync(this ScrollRect scrollRect, GameObject target, float duration, Vector2? offset = null, Ease ease = Ease.OutCubic)
+        {
+            var rt = scrollRect.transform as RectTransform;
+
+            rt.ForceRebuildLayoutGroup();
+
+            var p1 = (Vector2)rt.InverseTransformPoint(scrollRect.content.position);
+            var p2 = (Vector2)rt.InverseTransformPoint(target.transform.position);
+
+            var targetAnchoredPosition = scrollRect.content.anchoredPosition;
+
+            if (scrollRect.horizontal)
+            {
+                targetAnchoredPosition.x = p1.x - p2.x + (offset.HasValue ? offset.Value.x : 0f);
+            }
+
+            if (scrollRect.vertical)
+            {
+                targetAnchoredPosition.y = p1.y - p2.y + (offset.HasValue ? offset.Value.y : 0f);
+            }
+
+            await scrollRect.content.DOAnchorPos(targetAnchoredPosition, duration).SetEase(ease).ToUniTask();
         }
     }
 }
